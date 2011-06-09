b5bd57a43b107c840a6393a886969e6987d38b47
xaav <xaav@xaav.tk>
2011-06-09 07:40:41 -0700

    Added ErrorException handler.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -18,3 +18,7 @@ spl_autoload_register(function($class)
     }
 });
 
+function exception_error_handler($errno, $errstr, $errfile, $errline ) {
+    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+}
+set_error_handler("exception_error_handler");
