e317b4f8efc765b525cc473220d85cb9d5cc135a
Kirill chEbba Chebunin <iam@chebba.org>
2011-09-15 23:58:00 +0400

    Add Dowloader and Installer interfaces

---
+++ src/Composer/Installer/InstallerInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of Composer.
+ *
+ * (c) Nils Adermann <naderman@naderman.de>
+ *     Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Composer\Installer;
+
+use Composer\Downloader\DownloaderInterface;
+use Composer\Package\PackageInterface;
+
+/**
+ * Package Installer
+ * 
+ * @author Kirill chEbba Chebunin <iam@chebba.org>
+ */ 
+interface InstallerInterface
+{
+    /**
+     * Install package
+     *
+     * @param PackageInterface    $package
+     * @param DownloaderInterface $downloader
+     * @param string              $type
+     */
+    public function install(PackageInterface $package, DownloaderInterface $downloader, $type);
+}
