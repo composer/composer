136b6731fa31726ca2f3b14621d739fc1ea94279
Jordi Boggiano <j.boggiano@seld.be>
2011-10-02 23:54:13 +0200

    Remove validation for now since it's doing more harm than good

--- src/Composer/Package/Loader/ArrayLoader.php
+++ src/Composer/Package/Loader/ArrayLoader.php
@@ -41,10 +41,8 @@ class ArrayLoader
 
     public function load($config)
     {
-        $this->validateConfig($config);
-
-        $version = $this->versionParser->normalize($config['version']);
-        $package = new Package\MemoryPackage($config['name'], $version);
+        $version = $this->versionParser->normalize(isset($config['version']) ? $config['version'] : '0.0.0');
+        $package = new Package\MemoryPackage(isset($config['name']) ? $config['name'] : '__app__', $version);
 
         $package->setType(isset($config['type']) ? $config['type'] : 'library');
 
@@ -107,15 +105,5 @@ class ArrayLoader
         }
 
         return $links;
-    }
-
-    private function validateConfig(array $config)
-    {
-        if (!isset($config['name'])) {
-            throw new \UnexpectedValueException('name is required for package');
-        }
-        if (!isset($config['version'])) {
-            throw new \UnexpectedValueException('version is required for package');
-        }
     }
 }
