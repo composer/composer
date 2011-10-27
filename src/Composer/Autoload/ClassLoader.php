89caa9b117ac9186d96740fadbf354c27c1d4e8f
Jordi Boggiano <j.boggiano@seld.be>
2011-10-27 22:08:46 +0200

    Rewrite autoloader to use only PSR-0, remove splitting of PEAR path

---
+++ src/Composer/Autoload/ClassLoader.php
@@ -0,0 +1,117 @@
+<?php
+
+/*
+ * This file is part of Composer.
+ *
+ * (c) Nils Adermann <naderman@naderman.de>
+ *     Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Composer\Autoload;
+
+/**
+ * ClassLoader implements an PSR-0 class loader
+ *
+ * See https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-0.md
+ *
+ *     $loader = new ComposerClassLoader();
+ *
+ *     // register classes with namespaces
+ *     $loader->add('Symfony\Component' => __DIR__.'/component');
+ *     $loader->add('Symfony'           => __DIR__.'/framework');
+ *
+ *     // activate the autoloader
+ *     $loader->register();
+ *
+ * In this example, if you try to use a class in the Symfony\Component
+ * namespace or one of its children (Symfony\Component\Console for instance),
+ * the autoloader will first look for the class under the component/
+ * directory, and it will then fallback to the framework/ directory if not
+ * found before giving up.
+ *
+ * This class is loosely based on the Symfony UniversalClassLoader.
+ *
+ * @author Fabien Potencier <fabien@symfony.com>
+ * @author Jordi Boggiano <j.boggiano@seld.be>
+ */
+class ClassLoader
+{
+    private $prefixes = array();
+
+    public function getPrefixes()
+    {
+        return $this->prefixes;
+    }
+
+    /**
+     * Registers a set of classes
+     *
+     * @param string       $prefix  The classes prefix
+     * @param array|string $paths   The location(s) of the classes
+     */
+    public function add($prefix, $paths)
+    {
+        $this->prefixes[$prefix] = (array) $paths;
+    }
+
+    /**
+     * Registers this instance as an autoloader.
+     *
+     * @param Boolean $prepend Whether to prepend the autoloader or not
+     */
+    public function register($prepend = false)
+    {
+        spl_autoload_register(array($this, 'loadClass'), true, $prepend);
+    }
+
+    /**
+     * Loads the given class or interface.
+     *
+     * @param string $class The name of the class
+     */
+    public function loadClass($class)
+    {
+        if ($file = $this->findFile($class)) {
+            require $file;
+        }
+    }
+
+    /**
+     * Finds the path to the file where the class is defined.
+     *
+     * @param string $class The name of the class
+     *
+     * @return string|null The path, if found
+     */
+    public function findFile($class)
+    {
+        if ('\\' == $class[0]) {
+            $class = substr($class, 1);
+        }
+
+        if (false !== $pos = strrpos($class, '\\')) {
+            // namespaced class name
+            $namespace = substr($class, 0, $pos);
+            $className = substr($class, $pos + 1);
+        } else {
+            // PEAR-like class name
+            $namespace = null;
+            $className = $class;
+        }
+
+        foreach ($this->prefixes as $prefix => $dirs) {
+            foreach ($dirs as $dir) {
+                if (0 === strpos($class, $prefix)) {
+                    $file = $dir . DIRECTORY_SEPARATOR . ($namespace ? str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR : '')
+                        . str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+                    if (file_exists($file)) {
+                        return $file;
+                    }
+                }
+            }
+        }
+    }
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,17 +4,16 @@
  * This file is part of Composer.
  *
  * (c) Nils Adermann <naderman@naderman.de>
+ *     Jordi Boggiano <j.boggiano@seld.be>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
-spl_autoload_register(function($class)
-{
-    $file = __DIR__.'/../src/'.strtr($class, '\\', '/').'.php';
-    if (file_exists($file)) {
-        require $file;
-        return true;
-    }
-});
+require __DIR__.'/../src/Composer/Autoload/ClassLoader.php';
 
+$loader = new Composer\Autoload\ClassLoader();
+$loader->add('Composer\\', dirname(__DIR__).'/src/');
+$loader->add('Symfony\\Component\\', dirname(__DIR__).'/src/');
+$loader->add('Composer_', dirname(__DIR__).'/src/');
+$loader->register();
