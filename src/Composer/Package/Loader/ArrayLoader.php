595dc6f376a6e803127ae0612dacdaa8577ea0cc
Jordi Boggiano <j.boggiano@seld.be>
2011-11-03 19:32:28 +0100

    Stricter validation for the repositories config

--- src/Composer/Package/Loader/ArrayLoader.php
+++ src/Composer/Package/Loader/ArrayLoader.php
@@ -55,9 +55,12 @@ class ArrayLoader
 
         if (isset($config['repositories'])) {
             $repositories = array();
-            foreach ($config['repositories'] as $repo) {
-                if (!$repo) {
+            foreach ($config['repositories'] as $repoName => $repo) {
+                if (false === $repo && 'packagist' === $repoName) {
                     continue;
+                }
+                if (!is_array($repo)) {
+                    throw new \UnexpectedValueException('Repository '.$repoName.' in '.$package->getPrettyName().' '.$package->getVersion().' should be an array, '.gettype($repo).' given');
                 }
                 $repository = $this->manager->createRepository(key($repo), current($repo));
                 $this->manager->addRepository($repository);
