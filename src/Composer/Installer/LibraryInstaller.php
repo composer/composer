2ba48b58db0edc78d86c45bcad8db01defef0075
Jordi Boggiano <j.boggiano@seld.be>
2011-12-15 15:06:55 +0100

    Preserve case of packages for installations

--- src/Composer/Installer/LibraryInstaller.php
+++ src/Composer/Installer/LibraryInstaller.php
@@ -125,7 +125,7 @@ class LibraryInstaller implements InstallerInterface
     public function getInstallPath(PackageInterface $package)
     {
         $targetDir = $package->getTargetDir();
-        return ($this->vendorDir ? $this->vendorDir.'/' : '') . $package->getName() . ($targetDir ? '/'.$targetDir : '');
+        return ($this->vendorDir ? $this->vendorDir.'/' : '') . $package->getPrettyName() . ($targetDir ? '/'.$targetDir : '');
     }
 
     protected function installBinaries(PackageInterface $package)
