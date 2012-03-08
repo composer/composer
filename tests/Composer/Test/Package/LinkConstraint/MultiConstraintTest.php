23902023e5da6f51bfa0f4fb1c1319a3430ffe03
till <till@php.net>
2012-03-08 14:17:22 +0100

    fix namespace

--- tests/Composer/Test/Package/LinkConstraint/MultiConstraintTest.php
+++ tests/Composer/Test/Package/LinkConstraint/MultiConstraintTest.php
@@ -10,7 +10,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Composer\Test\Package;
+namespace Composer\Test\Package\LinkConstraint;
 
 use Composer\Package\LinkConstraint\VersionConstraint;
 use Composer\Package\LinkConstraint\MultiConstraint;
--- tests/Composer/Test/Repository/VcsRepositoryTest.php
+++ tests/Composer/Test/Repository/VcsRepositoryTest.php
@@ -10,7 +10,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Composer\Test\Json;
+namespace Composer\Test\Repository;
 
 use Symfony\Component\Process\ExecutableFinder;
 use Composer\Package\Dumper\ArrayDumper;
