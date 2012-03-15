eda840a9087bdf54944da14c3915aeea220c032f
Jordi Boggiano <j.boggiano@seld.be>
2012-03-15 13:14:02 +0100

    Regroup bootstrapers

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,18 +10,5 @@
  * file that was distributed with this source code.
  */
 
-function autoload($file) {
-    if (file_exists($file)) {
-        return include $file;
-    } else {
-        return false;
-    }
-}
-
-if ((!$loader = autoload(__DIR__.'/../../../.composer/autoload.php')) && (!$loader = autoload(__DIR__.'/../vendor/.composer/autoload.php'))) {
-    die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
-        'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
-        'php composer.phar install'.PHP_EOL);
-}
-
+$loader = require __DIR__.'/../src/bootstrap.php';
 $loader->add('Composer\Test', __DIR__);
