95d959e2cf5f1e5df641c6998ae36df67bf67b28
Marcin Chyłek <marcin@chylek.pl>
2012-03-15 01:51:54 +0100

    Add file_exists in bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,7 +10,15 @@
  * file that was distributed with this source code.
  */
 
-if ((!$loader = @include __DIR__.'/../../../.composer/autoload.php') && (!$loader = @include __DIR__.'/../vendor/.composer/autoload.php')) {
+function autoload($file) {
+    if (file_exists($file)) {
+        return include $file;
+    } else {
+        return false;
+    }
+}
+
+if ((!$loader = autoload(__DIR__.'/../../../.composer/autoload.php')) && (!$loader = autoload(__DIR__.'/../vendor/.composer/autoload.php'))) {
     die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
         'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
         'php composer.phar install'.PHP_EOL);
