b4d3d0911f2e5dfb0b7d1f2b6c5d9c253a263685
Jordi Boggiano <j.boggiano@seld.be>
2012-03-18 22:58:53 +0100

    Fix warning

--- src/Composer/Repository/VcsRepository.php
+++ src/Composer/Repository/VcsRepository.php
@@ -35,7 +35,7 @@ class VcsRepository extends ArrayRepository
 
         $this->url = $config['url'];
         $this->io = $io;
-        $this->type = $config['type'];
+        $this->type = isset($config['type']) ? $config['type'] : 'vcs';
     }
 
     public function setDebug($debug)
