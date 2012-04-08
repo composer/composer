6c78376094c344621ec3ca8fedd46c58115f9764
Jordi Boggiano <j.boggiano@seld.be>
2012-04-08 17:02:29 +0200

    Allow .x in version constraints as alias for .*

--- tests/Composer/Test/Package/Version/VersionParserTest.php
+++ tests/Composer/Test/Package/Version/VersionParserTest.php
@@ -154,8 +154,8 @@ class VersionParserTest extends \PHPUnit_Framework_TestCase
             array('2.*',     new VersionConstraint('>', '1.9999999.9999999.9999999'), new VersionConstraint('<', '2.9999999.9999999.9999999')),
             array('20.*',    new VersionConstraint('>', '19.9999999.9999999.9999999'), new VersionConstraint('<', '20.9999999.9999999.9999999')),
             array('2.0.*',   new VersionConstraint('>', '1.9999999.9999999.9999999'), new VersionConstraint('<', '2.0.9999999.9999999')),
-            array('2.2.*',   new VersionConstraint('>', '2.1.9999999.9999999'), new VersionConstraint('<', '2.2.9999999.9999999')),
-            array('2.10.*',  new VersionConstraint('>', '2.9.9999999.9999999'), new VersionConstraint('<', '2.10.9999999.9999999')),
+            array('2.2.x',   new VersionConstraint('>', '2.1.9999999.9999999'), new VersionConstraint('<', '2.2.9999999.9999999')),
+            array('2.10.x',  new VersionConstraint('>', '2.9.9999999.9999999'), new VersionConstraint('<', '2.10.9999999.9999999')),
             array('2.1.3.*', new VersionConstraint('>', '2.1.2.9999999'), new VersionConstraint('<', '2.1.3.9999999')),
             array('0.*',     null, new VersionConstraint('<', '0.9999999.9999999.9999999')),
         );
