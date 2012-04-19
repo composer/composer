871f10a2ea3b05362e5b4f9db9a791357d286f30
Jordi Boggiano <j.boggiano@seld.be>
2012-04-19 21:55:35 +0200

    Move .composer files out into the vendor dir, fixes #497

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -16,7 +16,7 @@ function includeIfExists($file) {
     }
 }
 
-if ((!$loader = includeIfExists(__DIR__.'/../vendor/.composer/autoload.php')) && (!$loader = includeIfExists(__DIR__.'/../../../.composer/autoload.php'))) {
+if ((!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php')) && (!$loader = includeIfExists(__DIR__.'/../../../autoload.php'))) {
     die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
         'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
         'php composer.phar install'.PHP_EOL);
