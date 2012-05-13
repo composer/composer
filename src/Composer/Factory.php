7d5feb5e1e9ac88800b552670e0474a4f888c7ed
Jordi Boggiano <j.boggiano@seld.be>
2012-05-13 11:35:50 +0200

    Add deprecation warning to old .composer files

--- src/Composer/Factory.php
+++ src/Composer/Factory.php
@@ -159,7 +159,7 @@ class Factory
 
     protected function addLocalRepository(RepositoryManager $rm, $vendorDir)
     {
-        // TODO BC feature, remove after May 30th
+        // TODO BC feature, remove after June 15th
         if (file_exists($vendorDir.'/.composer/installed.json')) {
             if (!is_dir($vendorDir.'/composer')) { mkdir($vendorDir.'/composer/', 0777, true); }
             rename($vendorDir.'/.composer/installed.json', $vendorDir.'/composer/installed.json');
