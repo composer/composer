31adcb1371b4db56892ac3a72f33addabc44158a
Nils Adermann <naderman@naderman.de>
2012-05-21 18:34:12 +0200

    Change "else if" to "elseif"

--- src/Composer/DependencyResolver/RuleSetGenerator.php
+++ src/Composer/DependencyResolver/RuleSetGenerator.php
@@ -204,7 +204,7 @@ class RuleSetGenerator
 
                     if (($package instanceof AliasPackage) && $package->getAliasOf() === $provider) {
                         $this->addRule(RuleSet::TYPE_PACKAGE, $rule = $this->createRequireRule($package, array($provider), Rule::RULE_PACKAGE_ALIAS, (string) $package));
-                    } else if (!$this->obsoleteImpossibleForAlias($package, $provider)) {
+                    } elseif (!$this->obsoleteImpossibleForAlias($package, $provider)) {
                         $reason = ($package->getName() == $provider->getName()) ? Rule::RULE_PACKAGE_SAME_NAME : Rule::RULE_PACKAGE_IMPLICIT_OBSOLETES;
                         $this->addRule(RuleSet::TYPE_PACKAGE, $rule = $this->createConflictRule($package, $provider, $reason, (string) $package));
                     }
