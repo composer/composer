71ac073ebe3db95e026a2081d2331bb7764a710b
Alexey Prilipko <palex@farpost.com>
2012-06-06 12:21:52 +1100

    Add 'include-path' option to PEAR packages

--- tests/Composer/Test/Repository/PearRepositoryTest.php
+++ tests/Composer/Test/Repository/PearRepositoryTest.php
@@ -12,7 +12,6 @@
 
 namespace Composer\Repository;
 
-use Composer\Repository\FilesystemRepository;
 use Composer\Test\TestCase;
 
 /**
@@ -30,10 +29,34 @@ class PearRepositoryTest extends TestCase
      */
     private $remoteFilesystem;
 
+    public function testComposerNonCompatibleRepositoryShouldSetIncludePath()
+    {
+        $url = 'pear.phpmd.org';
+        $expectedPackages = array(
+            array('name' => 'pear-phpmd/PHP_PMD', 'version' => '1.3.3'),
+        );
+
+        $repoConfig = array(
+            'url' => $url
+        );
+
+        $this->createRepository($repoConfig);
+
+        foreach ($expectedPackages as $expectedPackage) {
+            $package = $this->repository->findPackage($expectedPackage['name'], $expectedPackage['version']);
+            $this->assertInstanceOf('Composer\Package\PackageInterface',
+                $package,
+                'Expected package ' . $expectedPackage['name'] . ', version ' . $expectedPackage['version'] .
+                ' not found in pear channel ' . $url
+            );
+            $this->assertSame(array('/'), $package->getIncludePaths());
+        }
+    }
+
     /**
      * @dataProvider repositoryDataProvider
      * @param string $url
-     * @param array $expectedPackages
+     * @param array  $expectedPackages
      */
     public function testRepositoryRead($url, array $expectedPackages)
     {
