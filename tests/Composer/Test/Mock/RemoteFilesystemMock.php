3f84eac9cc623d582b79a6f47e84ef0cd8c152aa
Alexey Prilipko <palex@farpost.com>
2012-06-09 16:48:04 +1100

    Add Mock for RemoteFileSystem

---
+++ tests/Composer/Test/Mock/RemoteFilesystemMock.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of Composer.
+ *
+ * (c) Nils Adermann <naderman@naderman.de>
+ *     Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Composer\Test\Mock;
+
+use Composer\Util\RemoteFilesystem;
+
+/**
+ * Remote filesystem mock
+ */
+class RemoteFilesystemMock extends RemoteFilesystem
+{
+    /**
+     * @param array $contentMap associative array of locations and content
+     */
+    public function __construct(array $contentMap)
+    {
+        $this->contentMap = $contentMap;
+    }
+
+    public function getContents($originUrl, $fileUrl, $progress = true)
+    {
+        if(!empty($this->contentMap[$fileUrl]))
+            return $this->contentMap[$fileUrl];
+
+        throw new \Composer\Downloader\TransportException('The "'.$fileUrl.'" file could not be downloaded (NOT FOUND)', 404);
+    }
+
+}
