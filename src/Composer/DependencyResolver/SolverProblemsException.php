6573fd3f7790429420f10b159dfb587e2ddddf4b
Jordi Boggiano <j.boggiano@seld.be>
2012-07-04 15:57:51 +0200

    Add hint in solver exceptions as to what may be wrong

--- src/Composer/DependencyResolver/SolverProblemsException.php
+++ src/Composer/DependencyResolver/SolverProblemsException.php
@@ -35,6 +35,10 @@ class SolverProblemsException extends \RuntimeException
             $text .= "  Problem ".($i+1).$problem->getPrettyString($this->installedMap)."\n";
         }
 
+        if (strpos($text, 'could not be found') || strpos($text, 'no matching package found')) {
+            $text .= "\nPotential causes:\n - A typo in the package name\n - The package is not available in a stable-enough version according to your minimum-stability setting\n   see https://groups.google.com/d/topic/composer-dev/_g3ASeIFlrc/discussion for more details.\n";
+        }
+
         return $text;
     }
 
