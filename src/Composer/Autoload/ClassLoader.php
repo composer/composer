32c8bf3726d34dcfe0b084c785031de36fd60a0a
RWOverdijk <r.w.overdijk@gmail.com>
2012-08-02 11:00:46 +0200

    Feedback resolved

--- src/Composer/Autoload/ClassLoader.php
+++ src/Composer/Autoload/ClassLoader.php
@@ -148,6 +148,7 @@ class ClassLoader
     {
         if (($file = $this->findFile($class))) {
             include $file;
+
             return true;
         }
     }
@@ -180,7 +181,7 @@ class ClassLoader
         }
 
         $classPath .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-        
+
         foreach ($this->prefixes as $prefix => $dirs) {
             if (0 === strpos($class, $prefix)) {
                 foreach ($dirs as $dir) {
