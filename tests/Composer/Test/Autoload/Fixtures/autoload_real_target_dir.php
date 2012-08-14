4d35c6e8a889046fe2fcad2ef96f7500b60d5e33
Stéphane Goetz <onigoetz@onigoetz.ch>
2012-08-14 23:35:14 +0200

    Created a new autoloader able to manage all composer class maps at once

--- tests/Composer/Test/Autoload/Fixtures/autoload_real_target_dir.php
+++ tests/Composer/Test/Autoload/Fixtures/autoload_real_target_dir.php
@@ -6,27 +6,30 @@ require __DIR__ . '/ClassLoader.php';
 
 class ComposerAutoloaderInitTargetDir
 {
+    public static $loader;
+    
     public static function getLoader()
     {
-        $loader = new \Composer\Autoload\ClassLoader();
+        if(self::$loader == null){
+            self::$loader = new \Composer\Autoload\ClassLoader();
+            self::$loader->register();
+        }
         $vendorDir = dirname(__DIR__);
         $baseDir = dirname($vendorDir);
 
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
-            $loader->add($namespace, $path);
+            self::$loader->add($namespace, $path);
         }
 
         $classMap = require __DIR__ . '/autoload_classmap.php';
         if ($classMap) {
-            $loader->addClassMap($classMap);
+            self::$loader->addClassMap($classMap);
         }
 
         spl_autoload_register(array('ComposerAutoloaderInitTargetDir', 'autoload'));
 
-        $loader->register();
-
-        return $loader;
+        return self::$loader;
     }
 
     public static function autoload($class)
