e46d26cb9b6f77d4a6d7e0ae8ed33a529e753616
Jordi Boggiano <j.boggiano@seld.be>
2012-08-22 17:47:05 +0200

    Add loadAliasPackage to the StreamableRepositoryInterface and clear up responsibilities between Pool and Repositories

--- src/Composer/Repository/StreamableRepositoryInterface.php
+++ src/Composer/Repository/StreamableRepositoryInterface.php
@@ -12,6 +12,7 @@
 
 namespace Composer\Repository;
 
+use Composer\Package\AliasPackage;
 use Composer\Package\PackageInterface;
 
 /**
@@ -22,9 +23,33 @@ interface StreamableRepositoryInterface extends RepositoryInterface
     /**
      * Return partial package data without loading them all to save on memory
      *
+     * The package array must contain the following fields:
+     *  - name: package name (normalized/lowercased)
+     *  - repo: reference to the repository instance
+     *  - version: normalized version
+     *  - replace: array of package name => version constraint, optional
+     *  - provide: array of package name => version constraint, optional
+     *  - alias: pretty alias that this package should be aliased to, optional
+     *  - alias_normalized: normalized alias that this package should be aliased to, optional
+     *
      * @return array
      */
     public function getMinimalPackages();
 
-    public function loadPackage(array $data, $id);
+    /**
+     * Loads a package from minimal info of the package
+     *
+     * @param array $data the minimal info as was returned by getMinimalPackage
+     * @return PackageInterface
+     */
+    public function loadPackage(array $data);
+
+    /**
+     * Loads an alias package from minimal info of the package
+     *
+     * @param array            $data      the minimal info as was returned by getMinimalPackage
+     * @param PackageInterface $aliasOf   the package which this alias is an alias of
+     * @return AliasPackage
+     */
+    public function loadAliasPackage(array $data, PackageInterface $aliasOf);
 }
