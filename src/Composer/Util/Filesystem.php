b9697a10a4410683efabcc57d2a77dc486ed5d2c
Brian Zitzow <bzitzow@gmail.com>
2012-10-04 18:15:27 -0700

    WIP: Update Composer/Util/Filesystem. Add Filesystem object to Project Installer

--- src/Composer/Util/Filesystem.php
+++ src/Composer/Util/Filesystem.php
@@ -74,6 +74,35 @@ class Filesystem
         }
     }
 
+    public function directoryExists($dir)
+    {
+        if (!is_dir($dir)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function directoryIsEmpty($dir, $excluding= null)
+    {
+        if (!$this->directoryExists($dir)) {
+            throw new \RuntimeException("Directory \"$dir\" does not exist");
+        }
+
+        $excludeList = array('.', '..');
+        if (null !== $excluding) {
+
+        }
+
+        $dirContent = array_diff(scandir($dir), $excludeList);
+
+        if(count($dirContent) >= 1) {
+            return false;
+        }
+
+        return true;
+    }
+
     public function rename($source, $target)
     {
         if (true === @rename($source, $target)) {
