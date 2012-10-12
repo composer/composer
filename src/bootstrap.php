9c59248665b4b20e16d6f1b01beecfb5ff04bfc4
Pablo Godel <pgodel@gmail.com>
2012-10-12 17:01:54 -0400

    Added exit(1) when 'You must set up the project dependencies'

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -18,9 +18,10 @@ function includeIfExists($file)
 }
 
 if ((!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php')) && (!$loader = includeIfExists(__DIR__.'/../../../autoload.php'))) {
-    die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
+    echo 'You must set up the project dependencies, run the following commands:'.PHP_EOL.
         'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
-        'php composer.phar install'.PHP_EOL);
+        'php composer.phar install'.PHP_EOL;
+    exit(1);
 }
 
 return $loader;
