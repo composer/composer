e410da786e267e7bb22070b42ffdbe3104dc3f9d
Jordi Boggiano <j.boggiano@seld.be>
2012-10-18 16:39:47 +0200

    Add ConfigSourceInterface and matching class

---
+++ src/Composer/Config/ConfigSourceInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of Composer.
+ *
+ * (c) Nils Adermann <naderman@naderman.de>
+ *     Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Composer\Config;
+
+use Composer\Json\JsonManipulator;
+
+/**
+ * @author Jordi Boggiano <j.boggiano@seld.be>
+ */
+interface ConfigSourceInterface
+{
+    public function addRepository($name, $config);
+
+    public function removeRepository($name);
+
+    public function addConfigSetting($name, $value);
+
+    public function removeConfigSetting($name);
+}
