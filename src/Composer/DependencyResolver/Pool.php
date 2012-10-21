composer/composer

commit beb9a5bd727d49bf539db676f1aa946901090257

Code optimizations: avoid loops in match()

src/Composer/DependencyResolver/Pool.php
@@ -241,10 +241,6 @@ class Pool
             }
         }
 
-        if (!isset($this->packageByName[$name]) && !$candidates) {
-            return array();
-        }
-
         if (isset($this->packageByName[$name])) {
             $candidates = array_merge($candidates, $this->packageByName[$name]);
         }
@@ -390,16 +386,12 @@ class Pool
             $replaces = $candidate->getReplaces();
         }
 
-        foreach ($provides as $link) {
-            if ($link->getTarget() === $name && $constraint->matches($link->getConstraint())) {
-                return self::MATCH_PROVIDE;
-            }
+        if (isset($provides[$name]) && $constraint->matches($provides[$name]->getConstraint())) {
+            return self::MATCH_PROVIDE;
         }
 
-        foreach ($replaces as $link) {
-            if ($link->getTarget() === $name && $constraint->matches($link->getConstraint())) {
-                return self::MATCH_REPLACE;
-            }
+        if (isset($replaces[$name]) && $constraint->matches($replaces[$name]->getConstraint())) {
+            return self::MATCH_REPLACE;
         }
 
         return self::MATCH_NONE;

src/Composer/Package/Version/VersionParser.php
@@ -186,7 +186,7 @@ class VersionParser
             } else {
                 $parsedConstraint = $this->parseConstraints($constraint);
             }
-            $res[] = new Link($source, $target, $parsedConstraint, $description, $constraint);
+            $res[strtolower($target)] = new Link($source, $target, $parsedConstraint, $description, $constraint);
         }
 
         return $res;

tests/Composer/Test/Package/Loader/ArrayLoaderTest.php
@@ -34,7 +34,7 @@ class ArrayLoaderTest extends \PHPUnit_Framework_TestCase
 
         $package = $this->loader->load($config);
         $replaces = $package->getReplaces();
-        $this->assertEquals('== 1.2.3.4', (string) $replaces[0]->getConstraint());
+        $this->assertEquals('== 1.2.3.4', (string) $replaces['foo']->getConstraint());
     }
 
     public function testTypeDefault()

commit 4c1c3014b06bdfe76d8f4b8a9c86859d55e31b8f

Fix regression introduced in beb9a5bd727d49bf539db676f1aa946901090257

src/Composer/DependencyResolver/Pool.php
@@ -387,6 +387,23 @@ class Pool
             $replaces = $candidate->getReplaces();
         }
 
+        // aliases create multiple replaces/provides for one target so they can not use the shortcut
+        if (isset($replaces[0]) || isset($provides[0])) {
+            foreach ($provides as $link) {
+                if ($link->getTarget() === $name && $constraint->matches($link->getConstraint())) {
+                    return self::MATCH_PROVIDE;
+                }
+            }
+
+            foreach ($replaces as $link) {
+                if ($link->getTarget() === $name && $constraint->matches($link->getConstraint())) {
+                    return self::MATCH_REPLACE;
+                }
+            }
+
+            return self::MATCH_NONE;
+        }
+
         if (isset($provides[$name]) && $constraint->matches($provides[$name]->getConstraint())) {
             return self::MATCH_PROVIDE;
         }