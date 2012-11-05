aecf4ffe919406e387c818f27a6f8c47e29fc440
Jordi Boggiano <j.boggiano@seld.be>
2012-11-05 13:51:43 +0100

    Move creation of custom styles to the factory

--- src/Composer/Console/Application.php
+++ src/Composer/Console/Application.php
@@ -18,7 +18,6 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Formatter\OutputFormatter;
-use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 use Composer\Command;
 use Composer\Command\Helper\DialogHelper;
 use Composer\Composer;
@@ -63,8 +62,7 @@ class Application extends BaseApplication
     public function run(InputInterface $input = null, OutputInterface $output = null)
     {
         if (null === $output) {
-            $styles['highlight'] = new OutputFormatterStyle('red');
-            $styles['warning'] = new OutputFormatterStyle('black', 'yellow');
+            $styles = Factory::createAdditionalStyles();
             $formatter = new OutputFormatter(null, $styles);
             $output = new ConsoleOutput(ConsoleOutput::VERBOSITY_NORMAL, null, $formatter);
         }
