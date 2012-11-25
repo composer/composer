de3235a2a4c613523fd02c0145625f40ff2d8da3
Jordi Boggiano <j.boggiano@seld.be>
2012-11-25 14:10:58 +0100

    Add tests to prevent regressions on strings/nowdoc/comment stripping

--- tests/Composer/Test/Autoload/ClassMapGeneratorTest.php
+++ tests/Composer/Test/Autoload/ClassMapGeneratorTest.php
@@ -54,6 +54,7 @@ class ClassMapGeneratorTest extends \PHPUnit_Framework_TestCase
                 'Foo\\LargeClass'         => realpath(__DIR__).'/Fixtures/classmap/LargeClass.php',
                 'Foo\\LargeGap'           => realpath(__DIR__).'/Fixtures/classmap/LargeGap.php',
                 'Foo\\MissingSpace'       => realpath(__DIR__).'/Fixtures/classmap/MissingSpace.php',
+                'Foo\\StripNoise'         => realpath(__DIR__).'/Fixtures/classmap/StripNoise.php',
                 'Unicode\\↑\\↑'              => realpath(__DIR__).'/Fixtures/classmap/Unicode.php',
             )),
             array(__DIR__.'/Fixtures/template', array()),
