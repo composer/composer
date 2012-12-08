6b0911306698fbcf422c35b8f28c1db9d3c7d6c8
Jordi Boggiano <j.boggiano@seld.be>
2012-12-08 21:45:43 +0100

    Warn users if github tokens contain invalid chars

--- src/Composer/Factory.php
+++ src/Composer/Factory.php
@@ -199,6 +199,9 @@ class Factory
         // reload oauth token from config if available
         if ($tokens = $config->get('github-oauth')) {
             foreach ($tokens as $domain => $token) {
+                if (!preg_match('{^[a-z0-9]+$}', $token)) {
+                    throw new \UnexpectedValueException('Your github oauth token for '.$domain.' contains invalid characters: "'.$token.'"');
+                }
                 $io->setAuthentication($domain, $token, 'x-oauth-basic');
             }
         }
