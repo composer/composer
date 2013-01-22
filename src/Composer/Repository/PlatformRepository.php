53e69f75dffee6e4c40c94869c08ffa0f5d26fba
Daniel González <daniel.gonzalez@freelancemadrid.es>
2013-01-22 16:28:43 +0100

    rel #1506

--- src/Composer/Repository/PlatformRepository.php
+++ src/Composer/Repository/PlatformRepository.php
@@ -20,6 +20,7 @@ use Composer\Package\Version\VersionParser;
  */
 class PlatformRepository extends ArrayRepository
 {
+
     protected function initialize()
     {
         parent::initialize();
@@ -29,13 +30,19 @@ class PlatformRepository extends ArrayRepository
         try {
             $prettyVersion = PHP_VERSION;
             $version = $versionParser->normalize($prettyVersion);
-        } catch (\UnexpectedValueException $e) {
+        }
+        catch (\UnexpectedValueException $e) {
             $prettyVersion = preg_replace('#^([^~+-]+).*$#', '$1', PHP_VERSION);
             $version = $versionParser->normalize($prettyVersion);
         }
 
         $php = new CompletePackage('php', $version, $prettyVersion);
         $php->setDescription('The PHP interpreter');
+        parent::addPackage($php);
+
+        $operatingSystemBits = (int) PHP_INT_SIZE / 8;
+        $operatingSystem = new CompletePackage('OS', $operatingSystemBits, $operatingSystemBits . ' bits');
+        $operatingSystem->setDescription('The Openrating System');
         parent::addPackage($php);
 
         $loadedExtensions = get_loaded_extensions();
@@ -50,13 +57,14 @@ class PlatformRepository extends ArrayRepository
             try {
                 $prettyVersion = $reflExt->getVersion();
                 $version = $versionParser->normalize($prettyVersion);
-            } catch (\UnexpectedValueException $e) {
+            }
+            catch (\UnexpectedValueException $e) {
                 $prettyVersion = '0';
                 $version = $versionParser->normalize($prettyVersion);
             }
 
-            $ext = new CompletePackage('ext-'.$name, $version, $prettyVersion);
-            $ext->setDescription('The '.$name.' PHP extension');
+            $ext = new CompletePackage('ext-' . $name, $version, $prettyVersion);
+            $ext->setDescription('The ' . $name . ' PHP extension');
             parent::addPackage($ext);
         }
 
@@ -80,8 +88,8 @@ class PlatformRepository extends ArrayRepository
 
                 case 'openssl':
                     $prettyVersion = preg_replace_callback('{^(?:OpenSSL\s*)?([0-9.]+)([a-z]?).*}', function ($match) {
-                        return $match[1] . (empty($match[2]) ? '' : '.'.(ord($match[2]) - 96));
-                    }, OPENSSL_VERSION_TEXT);
+                                return $match[1] . (empty($match[2]) ? '' : '.' . (ord($match[2]) - 96));
+                            }, OPENSSL_VERSION_TEXT);
                     break;
 
                 case 'pcre':
@@ -103,13 +111,15 @@ class PlatformRepository extends ArrayRepository
 
             try {
                 $version = $versionParser->normalize($prettyVersion);
-            } catch (\UnexpectedValueException $e) {
+            }
+            catch (\UnexpectedValueException $e) {
                 continue;
             }
 
-            $lib = new CompletePackage('lib-'.$name, $version, $prettyVersion);
-            $lib->setDescription('The '.$name.' PHP library');
+            $lib = new CompletePackage('lib-' . $name, $version, $prettyVersion);
+            $lib->setDescription('The ' . $name . ' PHP library');
             parent::addPackage($lib);
         }
     }
+
 }
