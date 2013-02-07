735b59c1d6b5d2cc5d985b5d7867c6a902ea0e78
Nils Adermann <naderman@naderman.de>
2013-02-07 17:50:09 +0100

    Skip the vendor dir when archiving the current project

--- src/Composer/Command/ArchiveCommand.php
+++ src/Composer/Command/ArchiveCommand.php
@@ -78,6 +78,11 @@ EOT
             }
         } else {
             $package = $this->getComposer()->getPackage();
+
+            // also ignore the vendor dir
+            $excludes = $package->getArchiveExcludes();
+            $excludes[] = '/'.$this->getComposer()->getConfig()->get('vendor-dir');
+            $package->setArchiveExcludes($excludes);
         }
 
         $io->write('<info>Creating the archive.</info>');
