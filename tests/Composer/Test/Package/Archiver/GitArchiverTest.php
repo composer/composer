bcbc50c0d60bd7dff181a7838c29e83b57c8bab0
Nils Adermann <naderman@naderman.de>
2013-02-07 09:45:51 +0100

    Git can handle commit references in git archive just fine so use them

--- tests/Composer/Test/Package/Archiver/GitArchiverTest.php
+++ tests/Composer/Test/Package/Archiver/GitArchiverTest.php
@@ -31,7 +31,7 @@ class GitArchiverTest extends ArchiverTest
 
         // Test archive
         $archiver = new GitArchiver();
-        $archiver->archive($package->getSourceUrl(), $target, 'zip', 'master');
+        $archiver->archive($package->getSourceUrl(), $target, 'zip', 'master^1');
         $this->assertFileExists($target);
 
         unlink($target);
@@ -46,7 +46,7 @@ class GitArchiverTest extends ArchiverTest
 
         // Test archive
         $archiver = new GitArchiver();
-        $archiver->archive($package->getSourceUrl(), $target, 'tar', 'master');
+        $archiver->archive($package->getSourceUrl(), $target, 'tar', 'master^1');
         $this->assertFileExists($target);
 
         unlink($target);
@@ -73,6 +73,18 @@ class GitArchiverTest extends ArchiverTest
         }
 
         $result = $this->process->execute('git add b && git commit -m "commit b" -q');
+        if ($result > 0) {
+            chdir($currentWorkDir);
+            throw new \RuntimeException('Could not commit: '.$this->process->getErrorOutput());
+        }
+
+        $result = file_put_contents('d', 'c');
+        if (false === $result) {
+            chdir($currentWorkDir);
+            throw new \RuntimeException('Could not save file.');
+        }
+
+        $result = $this->process->execute('git add d && git commit -m "commit d" -q');
         if ($result > 0) {
             chdir($currentWorkDir);
             throw new \RuntimeException('Could not commit: '.$this->process->getErrorOutput());
