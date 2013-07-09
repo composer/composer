860483e97e0966823c1461876d72874d1ef4b061
Robert Schönthal <caziel@gmx.net>
2013-07-09 15:53:27 +0200

    reverted simplification cause of php < 5.3.6

--- src/Composer/Autoload/ClassMapGenerator.php
+++ src/Composer/Autoload/ClassMapGenerator.php
@@ -66,10 +66,9 @@ class ClassMapGenerator
         $map = array();
 
         foreach ($path as $file) {
-            /** @var \SplFileInfo $file */
             $filePath = $file->getRealPath();
 
-            if (!in_array($file->getExtension(), array('php', 'inc'))) {
+            if (!in_array(pathinfo($filePath, PATHINFO_EXTENSION), array('php', 'inc'))) {
                 continue;
             }
 
