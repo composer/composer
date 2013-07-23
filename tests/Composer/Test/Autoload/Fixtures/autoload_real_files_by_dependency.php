6f50e6dd5dcde6c0adfb57898f3263ca64eda5a6
Andreas Hennings <andreas@dqxtech.net>
2013-07-23 13:48:55 +0200

    Generate an autoload_files.php next to autoload_namespaces.php and autoload_classes.php

--- tests/Composer/Test/Autoload/Fixtures/autoload_real_files_by_dependency.php
+++ tests/Composer/Test/Autoload/Fixtures/autoload_real_files_by_dependency.php
@@ -38,12 +38,7 @@ class ComposerAutoloaderInitFilesAutoloadOrder
 
         $loader->register(true);
 
-        require $vendorDir . '/c/lorem/testC.php';
-        require $vendorDir . '/z/foo/testA.php';
-        require $vendorDir . '/d/d/testD.php';
-        require $vendorDir . '/b/bar/testB.php';
-        require $vendorDir . '/e/e/testE.php';
-        require $baseDir . '/root.php';
+        require __DIR__ . '/autoload_files.php';
 
         return $loader;
     }
