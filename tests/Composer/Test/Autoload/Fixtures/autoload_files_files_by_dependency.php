2e84672e2c93e49c2b3952174cbd109116b49b7a
Andreas Hennings <andreas@dqxtech.net>
2013-07-23 13:48:55 +0200

    Generate an autoload_files.php next to autoload_namespaces.php and autoload_classes.php

---
+++ tests/Composer/Test/Autoload/Fixtures/autoload_files_files_by_dependency.php
@@ -0,0 +1,8 @@
+<?php
+
+require $vendorDir . '/c/lorem/testC.php';
+require $vendorDir . '/z/foo/testA.php';
+require $vendorDir . '/d/d/testD.php';
+require $vendorDir . '/b/bar/testB.php';
+require $vendorDir . '/e/e/testE.php';
+require $baseDir . '/root.php';
