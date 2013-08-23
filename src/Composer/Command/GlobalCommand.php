4374d95baeb731f1d8dd859ac9074026210686e7
David Stoline <unncola@gmail.com>
2013-08-23 10:59:43 -0400

    Update help docs for the global command
    I noticed that COMPOSER_HOME/vendor/bin was wrong. Correcting.

--- src/Composer/Command/GlobalCommand.php
+++ src/Composer/Command/GlobalCommand.php
@@ -38,7 +38,7 @@ Use this command as a wrapper to run other Composer commands
 within the global context of COMPOSER_HOME.
 
 You can use this to install CLI utilities globally, all you need
-is to add the COMPOSER_HOME/vendor/bin dir to your PATH env var.
+is to add the COMPOSER_HOME/bin dir to your PATH env var.
 
 COMPOSER_HOME is c:\Users\<user>\AppData\Roaming\Composer on Windows
 and /home/<user>/.composer on unix systems.
