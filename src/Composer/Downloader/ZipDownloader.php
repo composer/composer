composer/composer

commit 72d4bea89eefdea82b402960020fb8dc9469cf2f

Change strategy for ZipDownloader
Try to use unzip command-line before ZipArchive as this one does not correctly handle file permissions whereas unzip does.

src/Composer/Downloader/ZipDownloader.php
@@ -33,6 +33,19 @@ class ZipDownloader extends ArchiveDownloader
 
     protected function extract($file, $path)
     {
+        $processError = null;
+
+        // try to use unzip on *nix
+        if (!defined('PHP_WINDOWS_VERSION_BUILD')) {
+
+            $command = 'unzip '.escapeshellarg($file).' -d '.escapeshellarg($path);
+            if (0 === $this->process->execute($command, $ignoredOutput)) {
+                return;
+            }
+
+            $processError = 'Failed to execute ' . $command . "\n\n" . $this->process->getErrorOutput();
+        }
+
         if (!class_exists('ZipArchive')) {
             // php.ini path is added to the error message to help users find the correct file
             $iniPath = php_ini_loaded_file();
@@ -43,19 +56,12 @@ class ZipDownloader extends ArchiveDownloader
                 $iniMessage = 'A php.ini file does not exist. You will have to create one.';
             }
 
-            $error = "You need the zip extension enabled to use the ZipDownloader.\n".
-                $iniMessage;
+            $error = "Could not decompress the archive, enable the PHP zip extension or install unzip.\n".
+                $iniMessage . "\n" . $processError;
 
-            // try to use unzip on *nix
             if (!defined('PHP_WINDOWS_VERSION_BUILD')) {
-                $command = 'unzip '.escapeshellarg($file).' -d '.escapeshellarg($path);
-                if (0 === $this->process->execute($command, $ignoredOutput)) {
-                    return;
-                }
-
-                $error = "Could not decompress the archive, enable the PHP zip extension or install unzip.\n".
-                    $iniMessage . "\n" .
-                    'Failed to execute ' . $command . "\n\n" . $this->process->getErrorOutput();
+                $error = "You need the zip extension enabled to use the ZipDownloader.\n".
+                    $iniMessage;
             }
 
             throw new \RuntimeException($error);

commit 7cec839d303e69e0d9395a680bb7933cc6ae19f2

Fix the ZipDownloader to catch the exceptions thrown in Symfony process. The problem was introduced in 72d4bea8 and causes composer to not fallback to ZipArchive when unzip can not be executed (i.e. when proc_open() is not allowed).

src/Composer/Downloader/ZipDownloader.php
@@ -39,11 +39,17 @@ class ZipDownloader extends ArchiveDownloader
         // try to use unzip on *nix
         if (!defined('PHP_WINDOWS_VERSION_BUILD')) {
             $command = 'unzip '.escapeshellarg($file).' -d '.escapeshellarg($path) . ' && chmod -R u+w ' . escapeshellarg($path);
-            if (0 === $this->process->execute($command, $ignoredOutput)) {
-                return;
-            }
+            try {
+                if (0 === $this->process->execute($command, $ignoredOutput)) {
+                    return;
+                }
 
-            $processError = 'Failed to execute ' . $command . "\n\n" . $this->process->getErrorOutput();
+                $processError = 'Failed to execute ' . $command . "\n\n" . $this->process->getErrorOutput();
+            }
+            catch(\Exception $e)
+            {
+                $processError = $e->getMessage();
+            }
         }
 
         if (!class_exists('ZipArchive')) {