4dfa3c469e04e5db02e4840ea23a1aa6264f0f9c
Jordi Boggiano <j.boggiano@seld.be>
2014-07-20 19:07:31 +0200

    Fix class map generator handling of missing files

--- src/Composer/Autoload/ClassMapGenerator.php
+++ src/Composer/Autoload/ClassMapGenerator.php
@@ -116,6 +116,14 @@ class ClassMapGenerator
 
         try {
             $contents = @php_strip_whitespace($path);
+            if (!$contents) {
+                if (!file_exists($path)) {
+                    throw new \Exception('File does not exist');
+                }
+                if (!is_readable($path)) {
+                    throw new \Exception('File is not readable');
+                }
+            }
         } catch (\Exception $e) {
             throw new \RuntimeException('Could not scan for classes inside '.$path.": \n".$e->getMessage(), 0, $e);
         }
