78a2dd53b0808803c37749cac0d0110b54f64959
Yegor Tokmakov <yegor.tokmakov@home24.de>
2014-09-26 15:16:10 +0200

    AutoloadGenerator ClassLoader check simplified

--- src/Composer/Autoload/AutoloadGenerator.php
+++ src/Composer/Autoload/AutoloadGenerator.php
@@ -457,22 +457,17 @@ class ComposerAutoloaderInit$suffix
 {
     private static \$loader;
 
-    public static function loadClassLoader(\$class)
-    {
-        if ('Composer\\Autoload\\ClassLoader' === \$class) {
-            require __DIR__ . '/ClassLoader.php';
-        }
-    }
-
     public static function getLoader()
     {
         if (null !== self::\$loader) {
             return self::\$loader;
         }
 
-        spl_autoload_register(array('ComposerAutoloaderInit$suffix', 'loadClassLoader'), true, $prependAutoloader);
+        if (!class_exists('\\Composer\\Autoload\\ClassLoader')) {
+            require __DIR__ . '/ClassLoader.php';
+        }
+
         self::\$loader = \$loader = new \\Composer\\Autoload\\ClassLoader();
-        spl_autoload_unregister(array('ComposerAutoloaderInit$suffix', 'loadClassLoader'));
 
 
 HEADER;
--- tests/Composer/Test/Autoload/Fixtures/autoload_real_functions.php
+++ tests/Composer/Test/Autoload/Fixtures/autoload_real_functions.php
@@ -6,22 +6,17 @@ class ComposerAutoloaderInitFilesAutoload
 {
     private static $loader;
 
-    public static function loadClassLoader($class)
-    {
-        if ('Composer\Autoload\ClassLoader' === $class) {
-            require __DIR__ . '/ClassLoader.php';
-        }
-    }
-
     public static function getLoader()
     {
         if (null !== self::$loader) {
             return self::$loader;
         }
 
-        spl_autoload_register(array('ComposerAutoloaderInitFilesAutoload', 'loadClassLoader'), true, true);
+        if (!class_exists('\Composer\Autoload\ClassLoader')) {
+            require __DIR__ . '/ClassLoader.php';
+        }
+
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
-        spl_autoload_unregister(array('ComposerAutoloaderInitFilesAutoload', 'loadClassLoader'));
 
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
