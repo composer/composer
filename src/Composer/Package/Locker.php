124ecda584299fc102bf9c452ca50f97aadc7f11
Ala Eddine Khefifi <alakhefifi@gmail.com>
2014-11-20 18:24:01 +0100

    Update Locker.php

--- src/Composer/Package/Locker.php
+++ src/Composer/Package/Locker.php
@@ -173,9 +173,11 @@ class Locker
         return isset($lockData['stability-flags']) ? $lockData['stability-flags'] : array();
     }
 
-     /*
+     /**
      * Return null if not set to allow caller logic to choose the right behavior 
      * since old lock files have no prefer-stable
+     * 
+     *  @return string|null
      */
     public function getPreferStable()
     {
