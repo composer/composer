939a4aa9c2d90159323dc1397d99b70063751a74
Padraic Brady <padraic.brady@gmail.com>
2015-01-29 20:59:27 +0000

    Get tests running to prep for fixes

--- src/Composer/Command/Command.php
+++ src/Composer/Command/Command.php
@@ -19,7 +19,6 @@ use Composer\IO\NullIO;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Command\Command as BaseCommand;
-use Symfony\Component\Console\Input\InputInterface;
 
 /**
  * Base class for Composer commands
