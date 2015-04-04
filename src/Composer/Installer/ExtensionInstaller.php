291ea96c49069acc8e7faaee73b74cce79cb714a
Pierre Joye <pierre.php@gmail.com>
2015-04-04 05:30:24 +0200

    restore header

--- src/Composer/Installer/ExtensionInstaller.php
+++ src/Composer/Installer/ExtensionInstaller.php
@@ -3,7 +3,8 @@
 /*
  * This file is part of Composer.
  *
- * (c) Pierre Joye <pierre.php@gmail.com>
+ * (c) Nils Adermann <naderman@naderman.de>
+ *     Jordi Boggiano <j.boggiano@seld.be>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
