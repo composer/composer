b2173d28fc8b56236eddc8aa10dcda61471633ec
Jordi Boggiano <j.boggiano@seld.be>
2015-06-16 11:43:55 +0100

    Fix 5.3.3 build

--- tests/Composer/Test/Autoload/ClassMapGeneratorTest.php
+++ tests/Composer/Test/Autoload/ClassMapGeneratorTest.php
@@ -27,6 +27,10 @@ class ClassMapGeneratorTest extends \PHPUnit_Framework_TestCase
 
     public function getTestCreateMapTests()
     {
+        if (PHP_VERSION_ID == 50303) {
+            $this->markTestSkipped('Test segfaults on travis 5.3.3 due to ClassMap\LongString');
+        }
+
         $data = array(
             array(__DIR__.'/Fixtures/Namespaced', array(
                 'Namespaced\\Bar' => realpath(__DIR__).'/Fixtures/Namespaced/Bar.inc',
