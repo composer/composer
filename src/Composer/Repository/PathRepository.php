23ad67b3bed09c5bfc807f76c611b6f6ee0ae59e
Niels Keurentjes <niels.keurentjes@omines.com>
2016-04-12 13:14:07 +0200

    Implement support for tilde expansion, mainly for path repositories

--- src/Composer/Repository/PathRepository.php
+++ src/Composer/Repository/PathRepository.php
@@ -18,6 +18,7 @@ use Composer\Json\JsonFile;
 use Composer\Package\Loader\ArrayLoader;
 use Composer\Package\Version\VersionGuesser;
 use Composer\Package\Version\VersionParser;
+use Composer\Util\Platform;
 use Composer\Util\ProcessExecutor;
 
 /**
@@ -101,7 +102,7 @@ class PathRepository extends ArrayRepository implements ConfigurableRepositoryIn
         }
 
         $this->loader = new ArrayLoader(null, true);
-        $this->url = $repoConfig['url'];
+        $this->url = Platform::expandPath($repoConfig['url']);
         $this->process = new ProcessExecutor($io);
         $this->versionGuesser = new VersionGuesser($config, $this->process, new VersionParser());
         $this->repoConfig = $repoConfig;
--- src/Composer/Util/Platform.php
+++ src/Composer/Util/Platform.php
@@ -20,10 +20,36 @@ namespace Composer\Util;
 class Platform
 {
     /**
+     * Parses magic constructs like tildes in paths. Right now only tildes are supported but we could add support for 
+     * environment variables on various platforms.
+     *
+     * @param string $path
+     * @return string
+     */
+    public static function expandPath($path)
+    {
+        // Tilde expansion for *nix
+        if (!self::isWindows() && 0 === strpos($path, '~/')) {
+            if (function_exists('posix_getuid') && function_exists('posix_getpwuid')) {
+                $info = posix_getpwuid(posix_getuid());
+                $home = $info['dir'];
+            } else {
+                $home = getenv('HOME');
+            }
+            // Cannot be empty or FALSE
+            if (!$home) {
+                throw new \RuntimeException(sprintf('No home folder found to expand ~ with in %s', $path));
+            }
+            $path = $home . substr($path, 1);
+        }
+        return $path;
+    }
+
+    /**
      * @return bool Whether the host machine is running a Windows OS
      */
     public static function isWindows()
     {
         return defined('PHP_WINDOWS_VERSION_BUILD');
     }
-}
+ }
