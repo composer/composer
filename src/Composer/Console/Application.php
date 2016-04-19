89cfde051d70a02b9335fd79ce41bb7b3090a365
Jordi Boggiano <j.boggiano@seld.be>
2016-04-19 11:00:39 +0100

    Convert exception to a warning, refs #5205

--- src/Composer/Console/Application.php
+++ src/Composer/Console/Application.php
@@ -160,7 +160,7 @@ class Application extends BaseApplication
             Silencer::call(function() {
                 $tempfile = sys_get_temp_dir() . '/temp-' . md5(microtime());
                 if (!(file_put_contents($tempfile, __FILE__) && (file_get_contents($tempfile) == __FILE__) && unlink($tempfile) && !file_exists($tempfile))) {
-                    throw new \RuntimeException(sprintf('PHP temp directory "%s" does not exist or is not writable to Composer - check sys_temp_dir in your php.ini', sys_get_temp_dir()));
+                    $io->writeError(sprintf('<error>PHP temp directory (%s) does not exist or is not writable to Composer. Set sys_temp_dir in your php.ini</error>', sys_get_temp_dir()));
                 }
             });
 
