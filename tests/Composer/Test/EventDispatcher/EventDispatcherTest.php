e6c6d25b599da56ccc17def8c82fe88d3130210e
Jordi Boggiano <j.boggiano@seld.be>
2016-04-24 15:55:03 +0100

    Fix tests

--- tests/Composer/Test/EventDispatcher/EventDispatcherTest.php
+++ tests/Composer/Test/EventDispatcher/EventDispatcherTest.php
@@ -50,6 +50,9 @@ class EventDispatcherTest extends TestCase
         $dispatcher->dispatchScript(ScriptEvents::POST_INSTALL_CMD, false);
     }
 
+    /**
+     * @expectedException PHPUnit_Framework_Error
+     */
     public function testDispatcherCanConvertScriptEventToCommandEventForListener()
     {
         $io = $this->getMock('Composer\IO\IOInterface');
