2175989cf134be79bcb818dd1ac050482392b4f7
AnrDaemon <anrdaemon@freemail.ru>
2016-07-20 08:27:33 +0300

    Fix failing realpath() on Windows.

--- src/Composer/Autoload/AutoloadGenerator.php
+++ src/Composer/Autoload/AutoloadGenerator.php
@@ -100,7 +100,7 @@ class AutoloadGenerator
         $filesystem = new Filesystem();
         $filesystem->ensureDirectoryExists($config->get('vendor-dir'));
         $basePath = $filesystem->normalizePath(realpath(getcwd()));
-        $vendorPath = $filesystem->normalizePath(realpath($config->get('vendor-dir')));
+        $vendorPath = $filesystem->normalizePath(realpath(realpath($config->get('vendor-dir'))));
         $useGlobalIncludePath = (bool) $config->get('use-include-path');
         $prependAutoloader = $config->get('prepend-autoloader') === false ? 'false' : 'true';
         $targetDir = $vendorPath.'/'.$targetDir;
