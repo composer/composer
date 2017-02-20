e16bc678219d0698de5174219a6f1322fd0667ee
Eugeniy Petrov <eug.a.petrov@gmail.com>
2017-02-20 10:45:51 +0100

    Check if xcache loaded before check settings

--- tests/Composer/Test/Autoload/Fixtures/autoload_real_include_path.php
+++ tests/Composer/Test/Autoload/Fixtures/autoload_real_include_path.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInitIncludePath
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitIncludePath', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded()) && ini_get('xcache.cacher') !== '1';
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded()) && extension_loaded('xcache') && ini_get('xcache.cacher') !== '1';
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
