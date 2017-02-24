618709c059ef2786c8f5834f44b9fad875d0cb45
Bernhard Froehlich <decke@bluelife.at>
2017-02-24 15:27:09 +0000

    Fix Repository support for Subversion 1.8.x where the output of svn info has changed.
    1.7.22
    svn: E170001: Unable to connect to a repository at URL 'https://svswdms02/dashboard'
    svn: E170001: OPTIONS of 'https://svswdms02/dashboard': authorization failed: Could not authenticate to server: rejected Digest challenge (https://svswdms02)
    
    1.8.17
    svn: E215004: Authentication failed and interactive prompting is disabled; see the --force-interactive option
    svn: E215004: Unable to connect to a repository at URL 'https://svswdms02/dashboard'
    svn: E215004: No more credentials or we tried too many times.
    Authentication failed

--- src/Composer/Repository/Vcs/SvnDriver.php
+++ src/Composer/Repository/Vcs/SvnDriver.php
@@ -301,8 +301,16 @@ class SvnDriver extends VcsDriver
             return true;
         }
 
+        // Subversion client 1.7 and older
         if (false !== stripos($processExecutor->getErrorOutput(), 'authorization failed:')) {
             // This is likely a remote Subversion repository that requires
+            // authentication. We will handle actual authentication later.
+            return true;
+        }
+
+        // Subversion client 1.8 and newer
+        if (false !== stripos($processExecutor->getErrorOutput(), 'Authentication failed')) {
+            // This is likely a remote Subversion or newer repository that requires
             // authentication. We will handle actual authentication later.
             return true;
         }
