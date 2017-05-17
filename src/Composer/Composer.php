489e09ee6c3ba431fbeeef9147afdaeb6f91b647
Jordi Boggiano <j.boggiano@seld.be>
2017-05-17 08:17:53 +0200

    Release 1.4.2

--- src/Composer/Composer.php
+++ src/Composer/Composer.php
@@ -28,9 +28,9 @@ use Composer\Autoload\AutoloadGenerator;
  */
 class Composer
 {
-    const VERSION = '@package_version@';
-    const BRANCH_ALIAS_VERSION = '@package_branch_alias_version@';
-    const RELEASE_DATE = '@release_date@';
+    const VERSION = '1.4.2';
+    const BRANCH_ALIAS_VERSION = '';
+    const RELEASE_DATE = '2017-05-17 08:17:52';
 
     /**
      * @var Package\RootPackageInterface
