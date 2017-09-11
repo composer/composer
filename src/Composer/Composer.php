c639623fa2178b404ed4bab80f0d1263853fa4ae
Jordi Boggiano <j.boggiano@seld.be>
2017-09-11 16:59:26 +0200

    Release 1.5.2

--- src/Composer/Composer.php
+++ src/Composer/Composer.php
@@ -29,9 +29,9 @@ use Composer\Package\Archiver\ArchiveManager;
  */
 class Composer
 {
-    const VERSION = '@package_version@';
-    const BRANCH_ALIAS_VERSION = '@package_branch_alias_version@';
-    const RELEASE_DATE = '@release_date@';
+    const VERSION = '1.5.2';
+    const BRANCH_ALIAS_VERSION = '';
+    const RELEASE_DATE = '2017-09-11 16:59:25';
 
     /**
      * @var Package\RootPackageInterface
