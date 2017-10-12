6da92e98acff05f746826e85942ff40f0d3ed6a6
croensch <christoph.roensch@hella-gutmann.com>
2017-10-12 16:35:05 +0200

    also support NO_PROXY
    since WINDOWS can only have one case and that is usually upperase

--- src/Composer/Util/StreamContextFactory.php
+++ src/Composer/Util/StreamContextFactory.php
@@ -55,8 +55,8 @@ final class StreamContextFactory
         }
 
         // Remove proxy if URL matches no_proxy directive
-        if (!empty($_SERVER['no_proxy']) && parse_url($url, PHP_URL_HOST)) {
-            $pattern = new NoProxyPattern($_SERVER['no_proxy']);
+        if (!empty($_SERVER['NO_PROXY']) || $_SERVER['no_proxy']) && parse_url($url, PHP_URL_HOST)) {
+            $pattern = new NoProxyPattern(!empty($_SERVER['no_proxy']) ? $_SERVER['no_proxy'] : $_SERVER['NO_PROXY']);
             if ($pattern->test($url)) {
                 unset($proxy);
             }
