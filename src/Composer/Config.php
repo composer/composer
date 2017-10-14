9c109dfea15eeca4bda5ecddb177d0ca060c317b
Zachary Flower <zach@ninjaninja.net>
2017-10-13 22:38:05 -0600

    Add default config source to Config class

--- src/Composer/Config.php
+++ src/Composer/Config.php
@@ -84,6 +84,7 @@ class Config
     private $configSource;
     /** @var ConfigSourceInterface */
     private $authConfigSource;
+    private $defaultsConfigSource;
     private $useEnvironment;
     private $warnedHosts = array();
 
@@ -118,6 +119,16 @@ class Config
     public function getAuthConfigSource()
     {
         return $this->authConfigSource;
+    }
+
+    public function setDefaultsConfigSource(ConfigSourceInterface $source)
+    {
+        $this->defaultsConfigSource = $source;
+    }
+
+    public function getDefaultsConfigSource()
+    {
+        return $this->defaultsConfigSource;
     }
 
     /**
