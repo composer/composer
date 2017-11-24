611396d6bca4cb96feef1717a4883a7ea36bf685
Niels Keurentjes <niels@keurentjes.nl>
2017-11-24 11:16:09 +0100

    Reword exception text to evoke somewhat less panic
    Refs https://github.com/composer/composer/issues/5196#issuecomment-346754244
    
    Signature mismatch could indicate MitM, or just a CDN issue which is rather more likely.

--- src/Composer/Repository/ComposerRepository.php
+++ src/Composer/Repository/ComposerRepository.php
@@ -677,7 +677,7 @@ class ComposerRepository extends ArrayRepository implements ConfigurableReposito
                     }
 
                     // TODO use scarier wording once we know for sure it doesn't do false positives anymore
-                    throw new RepositorySecurityException('The contents of '.$filename.' do not match its signature. This should indicate a man-in-the-middle attack. Try running composer again and report this if you think it is a mistake.');
+                    throw new RepositorySecurityException('The contents of '.$filename.' do not match its signature. This could indicate a man-in-the-middle attack. Try running composer again and report this if you think it is a mistake.');
                 }
 
                 $data = JsonFile::parseJson($json, $filename);
