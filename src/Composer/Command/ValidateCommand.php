ecb26c7b7521ec4f06b876c9c3ae3f2c8127f6b5
Markus Staab <maggus.staab@googlemail.com>
2017-11-30 15:39:00 +0100

    support COMPOSER env var in validate command (#6834)
    * support COMPOSER env var in validate command

--- src/Composer/Command/ValidateCommand.php
+++ src/Composer/Command/ValidateCommand.php
@@ -44,7 +44,7 @@ class ValidateCommand extends BaseCommand
                 new InputOption('no-check-publish', null, InputOption::VALUE_NONE, 'Do not check for publish errors'),
                 new InputOption('with-dependencies', 'A', InputOption::VALUE_NONE, 'Also validate the composer.json of all installed dependencies'),
                 new InputOption('strict', null, InputOption::VALUE_NONE, 'Return a non-zero exit code for warnings as well as errors'),
-                new InputArgument('file', InputArgument::OPTIONAL, 'path to composer.json file', './composer.json'),
+                new InputArgument('file', InputArgument::OPTIONAL, 'path to composer.json file'),
             ))
             ->setHelp(<<<EOT
 The validate command validates a given composer.json and composer.lock
@@ -66,7 +66,7 @@ EOT
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $file = $input->getArgument('file');
+        $file = $input->getArgument('file') ?: Factory::getComposerFile();
         $io = $this->getIO();
 
         if (!file_exists($file)) {
