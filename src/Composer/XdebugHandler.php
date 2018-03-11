f8dc77db1804dee7beace5a4ad2cc36578672744
johnstevenson <john-stevenson@blueyonder.co.uk>
2018-03-11 18:00:03 +0000

    Fix bc

--- src/Composer/XdebugHandler.php
+++ src/Composer/XdebugHandler.php
@@ -12,11 +12,20 @@
 
 namespace Composer;
 
-trigger_error('The ' . __NAMESPACE__ . '\XdebugHandler class is deprecated, use Composer\XdebugHandler\XdebugHandler instead.', E_USER_DEPRECATED);
+use Symfony\Component\Console\Output\OutputInterface;
+
+trigger_error('The ' . __NAMESPACE__ . '\XdebugHandler class is deprecated, use Composer\XdebugHandler\XdebugHandler instead,', E_USER_DEPRECATED);
 
 /**
  * @deprecated use Composer\XdebugHandler\XdebugHandler instead
  */
-class XdebugHandler
+class XdebugHandler extends XdebugHandler\XdebugHandler
 {
+    const ENV_ALLOW = 'COMPOSER_ALLOW_XDEBUG';
+    const ENV_VERSION = 'COMPOSER_XDEBUG_VERSION';
+
+    public function __construct(OutputInterface $output)
+    {
+        parent::__construct('composer', '--ansi');
+    }
 }
