composer/composer

commit 90ac5e0749a8b3040eb6edfa88f69b466b2ab4c9

improving doc

src/Composer/Command/StatusCommand.php
@@ -37,6 +37,9 @@ class StatusCommand extends BaseCommand
     const EXIT_CODE_UNPUSHED_CHANGES = 2;
     const EXIT_CODE_VERSION_CHANGES = 4;
 
+    /**
+     * @throws \Symfony\Component\Console\Exception\InvalidArgumentException
+     */
     protected function configure()
     {
         $this
@@ -55,6 +58,11 @@ EOT
         ;
     }
 
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|null
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         // init repos

commit 0f373e3249b0d9523c94766d69d47676f190fdb4

Fix issues introduced by #7191, fixes #7263

src/Composer/Command/StatusCommand.php
@@ -101,7 +101,9 @@ EOT
                 if ($changes = $downloader->getLocalChanges($package, $targetDir)) {
                     $errors[$targetDir] = $changes;
                 }
-            } elseif ($downloader instanceof VcsCapableDownloaderInterface) {
+            }
+
+            if ($downloader instanceof VcsCapableDownloaderInterface) {
                 if ($currentRef = $downloader->getVcsReference($package, $targetDir)) {
                     switch ($package->getInstallationSource()) {
                         case 'source':
@@ -129,13 +131,11 @@ EOT
                         );
                     }
                 }
-            } elseif ($downloader instanceof DvcsDownloaderInterface) {
+            }
+
+            if ($downloader instanceof DvcsDownloaderInterface) {
                 if ($unpushed = $downloader->getUnpushedChanges($package, $targetDir)) {
                     $unpushedChanges[$targetDir] = $unpushed;
-                }
-            } elseif ($downloader instanceof DownloaderInterface) {
-                if ($changes = $downloader->getLocalChanges($package, $targetDir)) {
-                    $errors[$targetDir] = $changes;
                 }
             }
         }

src/Composer/Downloader/DownloaderInterface.php
@@ -61,13 +61,4 @@ interface DownloaderInterface
      * @return DownloaderInterface
      */
     public function setOutputProgress($outputProgress);
-
-    /**
-     * Checks for changes to the local copy
-     *
-     * @param  PackageInterface $package package instance
-     * @param  string           $path    package directory
-     * @return string|null      changes or null
-     */
-    public function getLocalChanges(PackageInterface $package, $path);
 }

src/Composer/Downloader/FileDownloader.php
@@ -35,7 +35,7 @@ use Composer\Util\Url as UrlUtil;
  * @author François Pluchino <francois.pluchino@opendisplay.com>
  * @author Nils Adermann <naderman@naderman.de>
  */
-class FileDownloader implements DownloaderInterface
+class FileDownloader implements DownloaderInterface, ChangeReportInterface
 {
     protected $io;
     protected $config;