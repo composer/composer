70a1a6e5103bc8515e5d71fc529185ee15948cd6
Markus Staab <maggus.staab@googlemail.com>
2018-07-18 16:38:44 +0200

    Throw a RuntimeException when glob braces are used but not supported by the OS

--- src/Composer/Repository/PathRepository.php
+++ src/Composer/Repository/PathRepository.php
@@ -178,6 +178,8 @@ class PathRepository extends ArrayRepository implements ConfigurableRepositoryIn
         
         if (defined('GLOB_BRACE')) {
             $flags |= GLOB_BRACE;
+        } elseif (strpos($this->url, '{') !== false || strpos($this->url, '}') !== false) {
+            throw new \RuntimeException('The operating system does not support GLOB_BRACE which is required for the url '. $this->url);
         }
         
         // Ensure environment-specific path separators are normalized to URL separators
