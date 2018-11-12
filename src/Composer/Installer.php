38a34159ef6e2faf49d55fde3b4f571e206ff987
Mathias Brodala <mbrodala@pagemachine.de>
2018-11-12 11:32:19 +0100

    Dispatch "post-package-update" event after writing lock (#7766)
    Fixes #7765

--- src/Composer/Installer.php
+++ src/Composer/Installer.php
@@ -610,13 +610,13 @@ class Installer
                 }
             }
 
+            if ($this->executeOperations || $this->writeLock) {
+                $localRepo->write();
+            }
+
             $event = 'Composer\Installer\PackageEvents::POST_PACKAGE_'.strtoupper($jobType);
             if (defined($event) && $this->runScripts) {
                 $this->eventDispatcher->dispatchPackageEvent(constant($event), $this->devMode, $policy, $pool, $installedRepo, $request, $operations, $operation);
-            }
-
-            if ($this->executeOperations || $this->writeLock) {
-                $localRepo->write();
             }
         }
 
