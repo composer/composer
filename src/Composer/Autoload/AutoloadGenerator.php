3b6b63784fe5e11e5eaf469aebd6fa40e723403a
Den Girnyk <den.girnyk@gmail.com>
2019-01-17 18:11:32 +0200

    Fix: Keep replaced packages for autoload dumping with --no-dev

--- src/Composer/Autoload/AutoloadGenerator.php
+++ src/Composer/Autoload/AutoloadGenerator.php
@@ -940,9 +940,13 @@ INITIALIZER;
             $packageMap,
             function ($item) use ($include) {
                 $package = $item[0];
-                $name = $package->getName();
+                foreach ($package->getNames() as $name) {
+                    if (isset($include[$name])) {
+                        return true;
+                    }
+                }
 
-                return isset($include[$name]);
+                return false;
             }
         );
     }
