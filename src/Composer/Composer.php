b3a6f3932feec1150d8a73eaec92be4223bae1a9
Jordi Boggiano <j.boggiano@seld.be>
2019-01-29 14:01:51 +0100

    Release 1.8.1

--- src/Composer/Composer.php
+++ src/Composer/Composer.php
@@ -29,9 +29,9 @@ use Composer\Package\Archiver\ArchiveManager;
  */
 class Composer
 {
-    const VERSION = '@package_version@';
-    const BRANCH_ALIAS_VERSION = '@package_branch_alias_version@';
-    const RELEASE_DATE = '@release_date@';
+    const VERSION = '1.8.1';
+    const BRANCH_ALIAS_VERSION = '';
+    const RELEASE_DATE = '2019-01-29 14:01:51';
 
     /**
      * @var Package\RootPackageInterface
