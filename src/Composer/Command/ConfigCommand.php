94df55425596c0137d0aa14485bba2fb05e15de6
Jordi Boggiano <j.boggiano@seld.be>
2019-02-10 12:56:43 +0100

    Make sure config command output is also output on --quiet so that warnings can be hidden, fixes #7963

--- src/Composer/Command/ConfigCommand.php
+++ src/Composer/Command/ConfigCommand.php
@@ -21,6 +21,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Composer\Config;
 use Composer\Config\JsonConfigSource;
 use Composer\Factory;
+use Composer\IO\IOInterface;
 use Composer\Json\JsonFile;
 use Composer\Semver\VersionParser;
 use Composer\Package\BasePackage;
@@ -284,7 +285,7 @@ EOT
                 $value = json_encode($value);
             }
 
-            $this->getIO()->write($value);
+            $this->getIO()->write($value, true, IOInterface::QUIET);
 
             return 0;
         }
@@ -695,9 +696,9 @@ EOT
             }
 
             if (is_string($rawVal) && $rawVal != $value) {
-                $io->write('[<comment>' . $k . $key . '</comment>] <info>' . $rawVal . ' (' . $value . ')</info>');
+                $io->write('[<comment>' . $k . $key . '</comment>] <info>' . $rawVal . ' (' . $value . ')</info>', true, IOInterface::QUIET);
             } else {
-                $io->write('[<comment>' . $k . $key . '</comment>] <info>' . $value . '</info>');
+                $io->write('[<comment>' . $k . $key . '</comment>] <info>' . $value . '</info>', true, IOInterface::QUIET);
             }
         }
     }
