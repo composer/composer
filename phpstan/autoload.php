5b0b88cfa0ec3975f456f8adc2ae121ea400aa02
CZechBoY <thejabko@gmail.com>
2019-02-12 21:57:17 +0100

    removed stubs and php-extensions installation in travis

--- phpstan/autoload.php
+++ phpstan/autoload.php
@@ -2,12 +2,4 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-if (!extension_loaded('apcu')) {
-    require_once __DIR__ . '/stubs/apcu.php';
-}
-
-if (!extension_loaded('rar')) {
-    require_once __DIR__ . '/stubs/rar.php';
-}
-
 require_once __DIR__ . '/../src/bootstrap.php';
