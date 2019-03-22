e0eee771640c1f3860614df68c86884a117063d2
CZechBoY <thejabko@gmail.com>
2019-03-22 23:52:14 +0100

    ShowCommand: early return for no package matches

--- src/Composer/Command/ShowCommand.php
+++ src/Composer/Command/ShowCommand.php
@@ -542,9 +542,13 @@ EOT
         $repositorySet->addRepository($repos);
 
         $matchedPackage = null;
-        $package = null;
         $versions = array();
         $matches = $repositorySet->findPackages($name, $constraint);
+
+        if ($matches === array()) {
+        	return array(null, array());
+        }
+
         foreach ($matches as $index => $package) {
             // select an exact match if it is in the installed repo and no specific version was required
             if (null === $version && $installedRepo->hasPackage($package)) {
