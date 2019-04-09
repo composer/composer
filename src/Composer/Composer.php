949b116f9e7d98d8d276594fed74b580d125c0e6
Jordi Boggiano <j.boggiano@seld.be>
2019-04-09 17:46:48 +0200

    Release 1.8.5

--- src/Composer/Composer.php
+++ src/Composer/Composer.php
@@ -45,14 +45,14 @@ class Composer
      * const SOURCE_VERSION = '';
      *
      * source (git clone):
-     * const VERSION = '@package_version@';
-     * const BRANCH_ALIAS_VERSION = '@package_branch_alias_version@';
-     * const RELEASE_DATE = '@release_date@';
+     * const VERSION = '1.8.5';
+     * const BRANCH_ALIAS_VERSION = '';
+     * const RELEASE_DATE = '2019-04-09 17:46:47';
      * const SOURCE_VERSION = '1.8-dev+source';
      */
-    const VERSION = '@package_version@';
-    const BRANCH_ALIAS_VERSION = '@package_branch_alias_version@';
-    const RELEASE_DATE = '@release_date@';
+    const VERSION = '1.8.5';
+    const BRANCH_ALIAS_VERSION = '';
+    const RELEASE_DATE = '2019-04-09 17:46:47';
     const SOURCE_VERSION = '1.8-dev+source';
 
     public static function getVersion()
