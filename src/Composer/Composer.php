5b4fad9056b01bd17bf241fa794e5dcb4150b7ea
Jordi Boggiano <j.boggiano@seld.be>
2019-10-06 21:39:10 +0200

    Update target version for master branch

--- src/Composer/Composer.php
+++ src/Composer/Composer.php
@@ -53,7 +53,7 @@ class Composer
     const VERSION = '@package_version@';
     const BRANCH_ALIAS_VERSION = '@package_branch_alias_version@';
     const RELEASE_DATE = '@release_date@';
-    const SOURCE_VERSION = '1.9-dev+source';
+    const SOURCE_VERSION = '1.10-dev+source';
 
     public static function getVersion()
     {
