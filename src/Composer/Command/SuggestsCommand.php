4c8e41d9a922005360bad3889ac3fdcc350c2195
Tyson Andre <tandre@themeetgroup.com>
2019-10-15 21:21:25 -0400

    Fix misc phpdoc and strpos arg order nits
    https://www.php.net/strpos has the signature
    `strpos ( string $haystack , mixed $needle [, int $offset = 0 ] ) : int`
    (The needle is usually the constant)
    
    `strpos('/', $suggestion)` would only be `false` for `''` and `'/'`
    
    So the existing check would just not suggest **anything** that was
    already installed (from pecl, built-in, or composer).
    
    The intent seems to be to not suggest non-vendored php packages
    that were already installed. (b20cc22ebb1d9f8ed467f0ea353391be7442ad3d)

--- src/Composer/Command/SuggestsCommand.php
+++ src/Composer/Command/SuggestsCommand.php
@@ -90,7 +90,7 @@ EOT
                 continue;
             }
             foreach ($package['suggest'] as $suggestion => $reason) {
-                if (false === strpos('/', $suggestion) && null !== $platform->findPackage($suggestion, '*')) {
+                if (null !== $platform->findPackage($suggestion, '*')) {
                     continue;
                 }
                 if (!isset($installed[$suggestion])) {
