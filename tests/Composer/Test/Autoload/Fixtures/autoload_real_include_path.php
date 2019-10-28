8e367ab710a181e209ac1fda439d90845d7d6291
Markus Staab <maggus.staab@googlemail.com>
2019-10-28 17:54:07 +0100

    fix fixture

--- tests/Composer/Test/Autoload/Fixtures/autoload_real_include_path.php
+++ tests/Composer/Test/Autoload/Fixtures/autoload_real_include_path.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInitIncludePath
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
