64aaacc28d374ed7dd2bffa72173da31d3137d8c
Markus Staab <maggus.staab@googlemail.com>
2019-10-28 17:53:41 +0100

    fix fixture

--- tests/Composer/Test/Autoload/Fixtures/autoload_real_files_by_dependency.php
+++ tests/Composer/Test/Autoload/Fixtures/autoload_real_files_by_dependency.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInitFilesAutoloadOrder
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
