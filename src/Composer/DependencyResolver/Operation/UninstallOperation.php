73a5cc63cc631fe66fa1f532cf5965c2879aff3a
Jordi Boggiano <j.boggiano@seld.be>
2020-03-12 08:12:12 +0100

    Always use Removing for consistency between dry-run and actual install

--- src/Composer/DependencyResolver/Operation/UninstallOperation.php
+++ src/Composer/DependencyResolver/Operation/UninstallOperation.php
@@ -61,7 +61,7 @@ class UninstallOperation extends SolverOperation
      */
     public function show($lock)
     {
-        return ($lock ? 'Removing ' : 'Uninstalling ').$this->package->getPrettyName().' ('.$this->package->getFullPrettyVersion().')';
+        return 'Removing '.$this->package->getPrettyName().' ('.$this->package->getFullPrettyVersion().')';
     }
 
     /**
