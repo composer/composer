7a4d5b6aa30d2118af27c04f5e897b57156ccfa9
Jordi Boggiano <j.boggiano@seld.be>
2020-04-10 11:44:22 +0200

    Release 1.10.5

--- src/Composer/Composer.php
+++ src/Composer/Composer.php
@@ -45,14 +45,14 @@ class Composer
      * const SOURCE_VERSION = '';
      *
      * source (git clone):
-     * const VERSION = '@package_version@';
-     * const BRANCH_ALIAS_VERSION = '@package_branch_alias_version@';
-     * const RELEASE_DATE = '@release_date@';
+     * const VERSION = '1.10.5';
+     * const BRANCH_ALIAS_VERSION = '';
+     * const RELEASE_DATE = '2020-04-10 11:44:22';
      * const SOURCE_VERSION = '1.8-dev+source';
      */
-    const VERSION = '@package_version@';
-    const BRANCH_ALIAS_VERSION = '@package_branch_alias_version@';
-    const RELEASE_DATE = '@release_date@';
+    const VERSION = '1.10.5';
+    const BRANCH_ALIAS_VERSION = '';
+    const RELEASE_DATE = '2020-04-10 11:44:22';
     const SOURCE_VERSION = '1.10-dev+source';
 
     public static function getVersion()
