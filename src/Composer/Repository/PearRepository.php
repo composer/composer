5b54a9375151a059179e6de8c3a58a99d728fdb8
Jordi Boggiano <j.boggiano@seld.be>
2020-04-15 00:33:52 +0200

    Fix phpstan build

--- src/Composer/Repository/PearRepository.php
+++ src/Composer/Repository/PearRepository.php
@@ -36,7 +36,7 @@ use Composer\Factory;
  */
 class PearRepository extends ArrayRepository
 {
-    public function __construct(array $repoConfig, IOInterface $io, Config $config, HttpDownloader $httpDownloader, EventDispatcher $dispatcher = null)
+    public function __construct()
     {
         throw new \RuntimeException('The PEAR repository has been removed from Composer 2.0');
     }
