c41bb909ff60bd378c6a8c17fc11b7aeb412e705
Jordi Boggiano <j.boggiano@seld.be>
2020-04-21 11:18:02 +0200

    Add platform check file in phars, fixes #8813

--- src/Composer/Compiler.php
+++ src/Composer/Compiler.php
@@ -139,6 +139,7 @@ class Compiler
         $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/composer/autoload_files.php'));
         $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/composer/autoload_real.php'));
         $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/composer/autoload_static.php'));
+        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/composer/platform_check.php'));
         if (file_exists(__DIR__.'/../../vendor/composer/include_paths.php')) {
             $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/composer/include_paths.php'));
         }
