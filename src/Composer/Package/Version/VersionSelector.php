953b8fbca7295625bbfcf63a37fa30386d808b87
Robert Korulczyk <robert@korulczyk.pl>
2020-07-22 21:29:44 +0200

    Fix constraint generated by `composer require` for packages with four-digit version

--- src/Composer/Package/Version/VersionSelector.php
+++ src/Composer/Package/Version/VersionSelector.php
@@ -150,16 +150,18 @@ class VersionSelector
         $semanticVersionParts = explode('.', $version);
 
         // check to see if we have a semver-looking version
-        if (count($semanticVersionParts) == 4 && preg_match('{^0\D?}', $semanticVersionParts[3])) {
-            // remove the last parts (i.e. the patch version number and any extra)
+        if (count($semanticVersionParts) !== 4) {
+            return $prettyVersion;
+        }
+
+        // remove the last parts (i.e. the patch version number and any extra)
+        if (preg_match('{^0\D?}', $semanticVersionParts[3])) {
             if ($semanticVersionParts[0] === '0') {
                 unset($semanticVersionParts[3]);
             } else {
                 unset($semanticVersionParts[2], $semanticVersionParts[3]);
             }
             $version = implode('.', $semanticVersionParts);
-        } else {
-            return $prettyVersion;
         }
 
         // append stability flag if not default
