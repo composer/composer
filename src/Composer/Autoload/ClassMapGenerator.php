bb1cc1004151dcb4059a9415261fcf09bc8a376a
Lars Moelleken <lars.moelleken@vdmg-it.com>
2020-09-07 19:17:43 +0200

    fix code style (tabs -> spaces)

--- src/Composer/Autoload/ClassMapGenerator.php
+++ src/Composer/Autoload/ClassMapGenerator.php
@@ -206,121 +206,121 @@ class ClassMapGenerator
         return $validClasses;
     }
 
-	/**
+    /**
      * Extract the classes in the given file
-	 *
+     *
      * @param  string            $path The file to check
      * @throws \RuntimeException
      * @return array             The found classes
-	 */
+     */
     private static function findClasses($path)
-	{
-		// Use @ here instead of Silencer to actively suppress 'unhelpful' output
-		// @link https://github.com/composer/composer/pull/4886
-		$contents = @php_strip_whitespace($path);
-		if (!$contents) {
-			if (!file_exists($path)) {
-				$message = 'File at "%s" does not exist, check your classmap definitions';
-			} elseif (!is_readable($path)) {
-				$message = 'File at "%s" is not readable, check its permissions';
-			} elseif ('' === trim(file_get_contents($path))) {
-				// The input file was really empty and thus contains no classes
-				return array();
-			} else {
-				$message = 'File at "%s" could not be parsed as PHP, it may be binary or corrupted';
-			}
-			$error = error_get_last();
-			if (isset($error['message'])) {
-				$message .= PHP_EOL . 'The following message may be helpful:' . PHP_EOL . $error['message'];
-			}
-			throw new \RuntimeException(sprintf($message, $path));
-		}
+    {
+        // Use @ here instead of Silencer to actively suppress 'unhelpful' output
+        // @link https://github.com/composer/composer/pull/4886
+        $contents = @php_strip_whitespace($path);
+        if (!$contents) {
+            if (!file_exists($path)) {
+                $message = 'File at "%s" does not exist, check your classmap definitions';
+            } elseif (!is_readable($path)) {
+                $message = 'File at "%s" is not readable, check its permissions';
+            } elseif ('' === trim(file_get_contents($path))) {
+                // The input file was really empty and thus contains no classes
+                return array();
+            } else {
+                $message = 'File at "%s" could not be parsed as PHP, it may be binary or corrupted';
+            }
+            $error = error_get_last();
+            if (isset($error['message'])) {
+                $message .= PHP_EOL . 'The following message may be helpful:' . PHP_EOL . $error['message'];
+            }
+            throw new \RuntimeException(sprintf($message, $path));
+        }
 
-		$found = array();
-		if (stripos($contents, 'class') !== false) {
-			$found[] = 'class';
-		}
-		if (stripos($contents, 'interface') !== false) {
-			$found[] = 'interface';
-		}
-		if (
-			PHP_VERSION_ID >= 50400
-			&&
-			stripos($contents, 'trait') !== false
-		) {
-			$found[] = 'trait';
-		}
-		if (
-			defined('HHVM_VERSION') && version_compare(HHVM_VERSION, '3.3', '>=')
-			&&
-			stripos($contents, 'enum') !== false
-		) {;
-			$found[] = 'enum';
-		}
-		if ($found === array()) {
-			return array();
-		}
-		if (!preg_match('{\b(?:' . \implode('|', $found) . ')\s}i', $contents)) {
-			return array();
-		}
+        $found = array();
+        if (stripos($contents, 'class') !== false) {
+            $found[] = 'class';
+        }
+        if (stripos($contents, 'interface') !== false) {
+            $found[] = 'interface';
+        }
+        if (
+            PHP_VERSION_ID >= 50400
+            &&
+            stripos($contents, 'trait') !== false
+        ) {
+            $found[] = 'trait';
+        }
+        if (
+            defined('HHVM_VERSION') && version_compare(HHVM_VERSION, '3.3', '>=')
+            &&
+            stripos($contents, 'enum') !== false
+        ) {;
+            $found[] = 'enum';
+        }
+        if ($found === array()) {
+            return array();
+        }
+        if (!preg_match('{\b(?:' . \implode('|', $found) . ')\s}i', $contents)) {
+            return array();
+        }
 
-		// strip heredocs/nowdocs
-		if (strpos($contents, '<<<') !== false) {
-			$contents = preg_replace('{<<<[ \t]*([\'"]?)(\w+)\\1(?:\r\n|\n|\r)(?:.*?)(?:\r\n|\n|\r)(?:\s*)\\2(?=\s+|[;,.)])}s', 'null', $contents);
-		}
+        // strip heredocs/nowdocs
+        if (strpos($contents, '<<<') !== false) {
+            $contents = preg_replace('{<<<[ \t]*([\'"]?)(\w+)\\1(?:\r\n|\n|\r)(?:.*?)(?:\r\n|\n|\r)(?:\s*)\\2(?=\s+|[;,.)])}s', 'null', $contents);
+        }
 
-		// strip strings
-		$contents = preg_replace('{"[^"]*+(?:[^"]*+)*+"|\'[^\']*+(?:[^\']*+)*+\'}', 'null', $contents);
+        // strip strings
+        $contents = preg_replace('{"[^"]*+(?:[^"]*+)*+"|\'[^\']*+(?:[^\']*+)*+\'}', 'null', $contents);
 
-		// strip leading non-php code if needed
-		if (strpos($contents, '<?') !== 0) {
-			$contents = preg_replace('{^.+?<\?}s', '<?', $contents, 1, $replacements);
-			if ($replacements === 0) {
-				return array();
-			}
-		}
+        // strip leading non-php code if needed
+        if (strpos($contents, '<?') !== 0) {
+            $contents = preg_replace('{^.+?<\?}s', '<?', $contents, 1, $replacements);
+            if ($replacements === 0) {
+                return array();
+            }
+        }
 
-		// strip non-php blocks in the file
-		if (strpos($contents, '?>') !== false) {
-			$contents = preg_replace('{\?>(?:[^<]++|<(?!\?))*+<\?}', '?><?', $contents);
-		}
+        // strip non-php blocks in the file
+        if (strpos($contents, '?>') !== false) {
+            $contents = preg_replace('{\?>(?:[^<]++|<(?!\?))*+<\?}', '?><?', $contents);
+        }
 
-		// strip trailing non-php code if needed
-		$pos = strrpos($contents, '?>');
+        // strip trailing non-php code if needed
+        $pos = strrpos($contents, '?>');
         if (false !== $pos && false === strpos(substr($contents, $pos), '<?')) {
-			$contents = substr($contents, 0, $pos);
-		}
+            $contents = substr($contents, 0, $pos);
+        }
 
-		// strip comments if short open tags are in the file
-		if (preg_match('{(?:<\?)(?!(?:php|hh))}i', $contents)) {
-			$contents = preg_replace('{//.* | /\*(?:[^*]++|\*(?!/))*\*/}x', '', $contents);
-		}
+        // strip comments if short open tags are in the file
+        if (preg_match('{(?:<\?)(?!(?:php|hh))}i', $contents)) {
+            $contents = preg_replace('{//.* | /\*(?:[^*]++|\*(?!/))*\*/}x', '', $contents);
+        }
 
-		if (strpos($contents, 'namespace') !== false) {
-			$namespaceRegex = '| \b(?<![\$:>])(?P<ns>namespace) (?P<nsname>\s++[a-z_\x7f-\xff][a-z0-9_\x7f-\xff]*+(?:\s*+\\\\\s*+[a-z_\x7f-\xff][a-z0-9_\x7f-\xff]*+)*+)? \s*+ [\{;]';
-		} else {
-			$namespaceRegex = '';
-		}
+        if (strpos($contents, 'namespace') !== false) {
+            $namespaceRegex = '| \b(?<![\$:>])(?P<ns>namespace) (?P<nsname>\s++[a-z_\x7f-\xff][a-z0-9_\x7f-\xff]*+(?:\s*+\\\\\s*+[a-z_\x7f-\xff][a-z0-9_\x7f-\xff]*+)*+)? \s*+ [\{;]';
+        } else {
+            $namespaceRegex = '';
+        }
 
-		preg_match_all('{
-			(?:
-				\b(?<![\$:>])(?P<type>' . \implode('|', $found) . ') \s+ (?P<name>[a-z_\x7f-\xff:][a-z0-9_\x7f-\xff:\-]*+)
-				' . $namespaceRegex . '
-			)
-		}ix', $contents, $matches);
+        preg_match_all('{
+            (?:
+                \b(?<![\$:>])(?P<type>' . \implode('|', $found) . ') \s+ (?P<name>[a-z_\x7f-\xff:][a-z0-9_\x7f-\xff:\-]*+)
+                ' . $namespaceRegex . '
+            )
+        }ix', $contents, $matches);
 
         $classes = array();
-		$namespace = '';
+        $namespace = '';
 
-		for ($i = 0, $len = count($matches['type']); $i < $len; $i++) {
+        for ($i = 0, $len = count($matches['type']); $i < $len; $i++) {
             if (!empty($matches['ns'][$i])) {
                 $namespace = str_replace(array(' ', "\t", "\r", "\n"), '', $matches['nsname'][$i]) . '\\';
-			} else {
-				$name = $matches['name'][$i];
-				// skip anon classes extending/implementing
-				if ($name === 'extends' || $name === 'implements') {
-					continue;
-				}
+            } else {
+                $name = $matches['name'][$i];
+                // skip anon classes extending/implementing
+                if ($name === 'extends' || $name === 'implements') {
+                    continue;
+                }
                 if ($name[0] === ':') {
                     // This is an XHP class, https://github.com/facebook/xhp
                     $name = 'xhp'.substr(str_replace(array('-', ':'), array('_', '__'), $name), 1);
@@ -330,11 +330,11 @@ class ClassMapGenerator
                     // The regex above captures the colon, which isn't part of
                     // the class name.
                     $name = rtrim($name, ':');
-				}
+                }
                 $classes[] = ltrim($namespace . $name, '\\');
-			}
-		}
+            }
+        }
 
         return $classes;
-	}
+    }
 }
