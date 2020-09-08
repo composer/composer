a6cc92b39c796ec3fb9a360f9c0e578afc2fc96d
Jordi Boggiano <j.boggiano@seld.be>
2020-09-08 22:58:51 +0200

    Release 1.10.12

--- src/Composer/Composer.php
+++ src/Composer/Composer.php
@@ -50,10 +50,10 @@ class Composer
      * const RELEASE_DATE = '@release_date@';
      * const SOURCE_VERSION = '1.8-dev+source';
      */
-    const VERSION = '@package_version@';
-    const BRANCH_ALIAS_VERSION = '@package_branch_alias_version@';
-    const RELEASE_DATE = '@release_date@';
-    const SOURCE_VERSION = '1.10-dev+source';
+    const VERSION = '1.10.12';
+    const BRANCH_ALIAS_VERSION = '';
+    const RELEASE_DATE = '2020-09-08 22:58:51';
+    const SOURCE_VERSION = '';
 
     /**
      * Version number of the internal composer-runtime-api package
