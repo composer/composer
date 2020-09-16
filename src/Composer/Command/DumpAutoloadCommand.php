6899060f6ee1893d0e205c45120c8cc9264c35fe
Lars Strojny <lars.strojny@internations.org>
2020-09-16 10:54:52 +0200

    Remove superfluous use

--- src/Composer/Command/DumpAutoloadCommand.php
+++ src/Composer/Command/DumpAutoloadCommand.php
@@ -14,7 +14,6 @@ namespace Composer\Command;
 
 use Composer\Plugin\CommandEvent;
 use Composer\Plugin\PluginEvents;
-use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
