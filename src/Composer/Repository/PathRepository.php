4feed8b85cf560ce52dfae6cff207c28dee30a99
Nicolas Grekas <nicolas.grekas@gmail.com>
2020-10-13 15:16:39 +0200

    Fix parsing "branch-version"

--- src/Composer/Repository/PathRepository.php
+++ src/Composer/Repository/PathRepository.php
@@ -167,7 +167,7 @@ class PathRepository extends ArrayRepository implements ConfigurableRepositoryIn
 
             // use the branch-version as the package version if available
             if (!isset($package['version']) && isset($package['extra']['branch-version'])) {
-                $package['version'] = preg_replace('{(\.x)?(-dev)?$}', '.x-dev', $package['extra']['branch-version']);
+                $package['version'] = preg_replace('{(\.x)?(-dev)?$}', '', $package['extra']['branch-version']).'.x-dev';
             }
 
             // carry over the root package version if this path repo is in the same git repository as root package
