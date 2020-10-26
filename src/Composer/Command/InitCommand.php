cbcc3a3080a40a237e9922045c6603d8d546693b
Nils Adermann <naderman@naderman.de>
2020-10-26 11:42:41 +0100

    Remove trailing commas for function calls

--- src/Composer/Command/InitCommand.php
+++ src/Composer/Command/InitCommand.php
@@ -743,7 +743,7 @@ EOT
                 throw new \InvalidArgumentException(sprintf(
                     'Package %s%s has a PHP requirement incompatible with your PHP version, PHP extensions and Composer version',
                     $name,
-                    $requiredVersion ? ' at version '.$requiredVersion : '',
+                    $requiredVersion ? ' at version '.$requiredVersion : ''
                 ));
             }
             // Check whether the required version was the problem
@@ -751,7 +751,7 @@ EOT
                 throw new \InvalidArgumentException(sprintf(
                     'Could not find package %s in a version matching %s',
                     $name,
-                    $requiredVersion ?: '*',
+                    $requiredVersion ?: '*'
                 ));
             }
             // Check whether the PHP version was the problem for all versions
