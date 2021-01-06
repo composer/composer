a5d79db0a70cfd6109f3bdbc9caf6346bc326cf9
Wissem Riahi <wissemr@gmail.com>
2021-01-06 18:00:48 +0100

    Make sure total_time_us from curl progress is set

--- src/Composer/Util/Http/CurlDownloader.php
+++ src/Composer/Util/Http/CurlDownloader.php
@@ -305,7 +305,7 @@ class CurlDownloader
                     }
 
                     $exception = new TransportException('curl error '.$errno.' while downloading '.Url::sanitize($progress['url']).': '.$error);
-                    $exception->setTotalResponseTime($progress['total_time_us']);
+                    $exception->setTotalResponseTime(isset($progress['total_time_us']) ? $progress['total_time_us'] : null);
                     throw $exception;
                 }
                 $statusCode = $progress['http_code'];
@@ -325,13 +325,13 @@ class CurlDownloader
                         $contents = stream_get_contents($job['bodyHandle']);
                     }
                     $response = new Response(array('url' => $progress['url']), $statusCode, $headers, $contents);
-                    $response->setTotalResponseTime($progress['total_time_us']);
+                    $response->setTotalResponseTime(isset($progress['total_time_us']) ? $progress['total_time_us'] : null);
                     $this->io->writeError('['.$statusCode.'] '.Url::sanitize($progress['url']), true, IOInterface::DEBUG);
                 } else {
                     rewind($job['bodyHandle']);
                     $contents = stream_get_contents($job['bodyHandle']);
                     $response = new Response(array('url' => $progress['url']), $statusCode, $headers, $contents);
-                    $response->setTotalResponseTime($progress['total_time_us']);
+                    $response->setTotalResponseTime(isset($progress['total_time_us']) ? $progress['total_time_us'] : null);
                     $this->io->writeError('['.$statusCode.'] '.Url::sanitize($progress['url']), true, IOInterface::DEBUG);
                 }
                 fclose($job['bodyHandle']);
