27d0c5c3ef621ba79d815e049ff56f3871ef2e81
Jordi Boggiano <j.boggiano@seld.be>
2021-02-12 11:12:42 +0100

    Add test deficiencies in installed versions as the runtime autoloaders are not prepended and do not grant access to the latest data

---
+++ tests/Composer/Test/Fixtures/functional/installed-versions/plugin-a/PluginA.php
@@ -0,0 +1,24 @@
+<?php
+
+use Composer\Plugin\PluginInterface;
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Json\JsonFile;
+use Composer\InstalledVersions;
+
+class PluginA implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        echo '!!PluginA:'.InstalledVersions::getVersion('plugin/a').JsonFile::encode(InstalledVersions::getInstalledPackages(), 320)."\n";
+        echo '!!Versions:console:'.InstalledVersions::getVersion('symfony/console').';process:'.InstalledVersions::getVersion('symfony/process').';filesystem:'.InstalledVersions::getVersion('symfony/filesystem')."\n";
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
+}
---
+++ tests/Composer/Test/Fixtures/functional/installed-versions/plugin-b/PluginB.php
@@ -0,0 +1,24 @@
+<?php
+
+use Composer\Plugin\PluginInterface;
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Json\JsonFile;
+use Composer\InstalledVersions;
+
+class PluginB implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        echo '!!PluginB:'.InstalledVersions::getVersion('plugin/b').JsonFile::encode(InstalledVersions::getInstalledPackages(), 320)."\n";
+        echo '!!Versions:console:'.InstalledVersions::getVersion('symfony/console').';process:'.InstalledVersions::getVersion('symfony/process').';filesystem:'.InstalledVersions::getVersion('symfony/filesystem')."\n";
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
+}
