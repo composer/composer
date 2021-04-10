dfdaf668a4555d057d0ed3ab0558ce1c651107f4
Markus Staab <markus.staab@redaxo.de>
2021-04-10 08:56:15 +0200

    revert

--- src/Composer/Command/RunScriptCommand.php
+++ src/Composer/Command/RunScriptCommand.php
@@ -19,7 +19,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Process\Exception\ProcessTimedOutException;
 
 /**
  * @author Fabien Potencier <fabien.potencier@gmail.com>
--- src/Composer/Command/ScriptAliasCommand.php
+++ src/Composer/Command/ScriptAliasCommand.php
@@ -16,7 +16,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Process\Exception\ProcessTimedOutException;
 
 /**
  * @author Jordi Boggiano <j.boggiano@seld.be>
