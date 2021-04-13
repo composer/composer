c3b76a85329c60220588e861eee02c2d0150c5ce
John Stevenson <john-stevenson@blueyonder.co.uk>
2021-04-13 08:59:06 +0100

    Upgrade to xdebug-handler 2 (#9832)
    This adds support for Xdebug3 modes and changes the default behaviour
    from always restarting if Xdebug is loaded, to only restarting if Xdebug
    is active.
    
    Xdebug is considered active if it is loaded, and for Xdebug3, if it is
    running in a mode other than `xdebug.mode=off`.

--- tests/Composer/Test/ApplicationTest.php
+++ tests/Composer/Test/ApplicationTest.php
@@ -13,6 +13,7 @@
 namespace Composer\Test;
 
 use Composer\Console\Application;
+use Composer\XdebugHandler\XdebugHandler;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ApplicationTest extends TestCase
@@ -54,7 +55,7 @@ class ApplicationTest extends TestCase
         $outputMock->expects($this->at($index++))
             ->method("write");
 
-        if (extension_loaded('xdebug')) {
+        if (XdebugHandler::isXdebugActive()) {
             $outputMock->expects($this->at($index++))
                 ->method("getVerbosity")
                 ->willReturn(OutputInterface::VERBOSITY_NORMAL);
