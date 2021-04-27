986e8b86b7b570632ad0a905c3726c33dd4c0efb
Jordi Boggiano <j.boggiano@seld.be>
2021-04-27 13:11:08 +0200

    Release 2.0.13

--- src/Composer/Composer.php
+++ src/Composer/Composer.php
@@ -51,10 +51,10 @@ class Composer
      * const RELEASE_DATE = '@release_date@';
      * const SOURCE_VERSION = '1.8-dev+source';
      */
-    const VERSION = '@package_version@';
-    const BRANCH_ALIAS_VERSION = '@package_branch_alias_version@';
-    const RELEASE_DATE = '@release_date@';
-    const SOURCE_VERSION = '2.0.999-dev+source';
+    const VERSION = '2.0.13';
+    const BRANCH_ALIAS_VERSION = '';
+    const RELEASE_DATE = '2021-04-27 13:11:08';
+    const SOURCE_VERSION = '';
 
     /**
      * Version number of the internal composer-runtime-api package
