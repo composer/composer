8ce0ea300e72e6528670e7cd4e8d2781fe6adae1
Jordi Boggiano <j.boggiano@seld.be>
2021-08-18 14:22:41 +0200

    Fix pear repo preventing self-update from functioning, fixes #10049

--- src/Composer/Repository/PearRepository.php
+++ src/Composer/Repository/PearRepository.php
@@ -27,6 +27,6 @@ class PearRepository extends ArrayRepository
 {
     public function __construct()
     {
-        throw new \RuntimeException('The PEAR repository has been removed from Composer 2.0');
+        throw new \InvalidArgumentException('The PEAR repository has been removed from Composer 2.x');
     }
 }
