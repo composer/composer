6c9aecf4e7c249ad4745fe20850adf6040062911
Jordi Boggiano <j.boggiano@seld.be>
2021-09-03 21:38:44 +0200

    Ignore all symfony deprecations

--- src/Composer/Util/ErrorHandler.php
+++ src/Composer/Util/ErrorHandler.php
@@ -53,8 +53,12 @@ class ErrorHandler
         }
 
         if (self::$io) {
-            // ignore symfony/* deprecation warnings about return types
+            // ignore symfony/* deprecation warnings
+            // TODO remove in 2.3
             if (preg_match('{^Return type of Symfony\\\\.*ReturnTypeWillChange}is', $message)) {
+                return true;
+            }
+            if (strpos(strtr($file, '\\', '/'), 'vendor/symfony/') !== false) {
                 return true;
             }
 
