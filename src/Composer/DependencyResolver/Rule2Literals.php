77c2cb0b26e8626268fa107e480cc8e2ea717c0f
imme-emosol <will+developer@willfris.nl>
2021-10-18 17:23:52 +0200

    Finishing touches?

--- src/Composer/DependencyResolver/Rule2Literals.php
+++ src/Composer/DependencyResolver/Rule2Literals.php
@@ -14,6 +14,7 @@ namespace Composer\DependencyResolver;
 
 /**
  * @author Nils Adermann <naderman@naderman.de>
+ * @phpstan-import-type ReasonData from Rule
  */
 class Rule2Literals extends Rule
 {
