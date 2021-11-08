7b7ec40a667c2f1dd05ad7535615d9f9d232f603
Cyril VERLOOP <cyril.verloop@webqard.fr>
2021-11-08 21:18:23 +0100

    Expanding ~ (tilde) in vcs url for private repositories.

--- src/Composer/Util/Git.php
+++ src/Composer/Util/Git.php
@@ -301,6 +301,7 @@ class Git
         $this->filesystem->removeDirectory($dir);
 
         $commandCallable = function ($url) use ($dir) {
+            $url = Platform::expandPath($url);
             return sprintf('git clone --mirror -- %s %s', ProcessExecutor::escape($url), ProcessExecutor::escape($dir));
         };
 
