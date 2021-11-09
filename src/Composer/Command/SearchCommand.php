85f3bb070d874dab84e35cfe93505a39372a556f
Jordi Boggiano <j.boggiano@seld.be>
2021-11-09 16:04:21 +0100

    Add abandoned flag in search command output

--- src/Composer/Command/SearchCommand.php
+++ src/Composer/Command/SearchCommand.php
@@ -93,12 +93,13 @@ EOT
             $nameLength += 1;
             foreach ($results as $result) {
                 $description = isset($result['description']) ? $result['description'] : '';
-                $remaining = $width - $nameLength - 2;
+                $warning = !empty($result['abandoned']) ? '<warning>! Abandoned !</warning> ' : '';
+                $remaining = $width - $nameLength - strlen($warning) - 2;
                 if (strlen($description) > $remaining) {
                     $description = substr($description, 0, $remaining - 3) . '...';
                 }
 
-                $io->write(str_pad($result['name'], $nameLength, ' ') . $description);
+                $io->write(str_pad($result['name'], $nameLength, ' ') . $warning . $description);
             }
         } elseif ($format === 'json') {
             $io->write(JsonFile::encode($results));
