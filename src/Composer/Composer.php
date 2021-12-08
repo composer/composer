d2ede370be20aedfc74749bc87394a79ed4c1e52
Jordi Boggiano <j.boggiano@seld.be>
2021-12-08 16:13:33 +0100

    Bump target version, bump PHP and dependency requirements to PHP 7.2+

--- src/Composer/Composer.php
+++ src/Composer/Composer.php
@@ -55,7 +55,7 @@ class Composer
     const VERSION = '@package_version@';
     const BRANCH_ALIAS_VERSION = '@package_branch_alias_version@';
     const RELEASE_DATE = '@release_date@';
-    const SOURCE_VERSION = '2.2.999-dev+source';
+    const SOURCE_VERSION = '2.3.999-dev+source';
 
     /**
      * Version number of the internal composer-runtime-api package
