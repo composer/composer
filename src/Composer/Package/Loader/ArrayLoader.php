07f4a47b3f6020fe00209caf57e134ea86ba972e
Stephan Vock <stephan.vock@gmail.com>
2022-01-19 10:19:36 +0000

    ArrayLoader: handle non string values for version/version_normalized

--- src/Composer/Package/Loader/ArrayLoader.php
+++ src/Composer/Package/Loader/ArrayLoader.php
@@ -113,12 +113,12 @@ class ArrayLoader implements LoaderInterface
         if (!isset($config['name'])) {
             throw new \UnexpectedValueException('Unknown package has no name defined ('.json_encode($config).').');
         }
-        if (!isset($config['version'])) {
+        if (!isset($config['version']) || !is_scalar($config['version'])) {
             throw new \UnexpectedValueException('Package '.$config['name'].' has no version defined.');
         }
 
         // handle already normalized versions
-        if (isset($config['version_normalized'])) {
+        if (isset($config['version_normalized']) && is_string($config['version_normalized'])) {
             $version = $config['version_normalized'];
 
             // handling of existing repos which need to remain composer v1 compatible, in case the version_normalized contained VersionParser::DEFAULT_BRANCH_ALIAS, we renormalize it
@@ -129,7 +129,7 @@ class ArrayLoader implements LoaderInterface
             $version = $this->versionParser->normalize($config['version']);
         }
 
-        return new $class($config['name'], $version, $config['version']);
+        return new $class($config['name'], $version, (string) $config['version']);
     }
 
     /**
