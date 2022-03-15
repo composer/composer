890b8fad3de8dc86d98a9cf0f578d86cb591205d
Jordi Boggiano <j.boggiano@seld.be>
2022-03-15 10:01:23 +0100

    Ignore phpstan issue

--- src/Composer/Installer.php
+++ src/Composer/Installer.php
@@ -473,7 +473,8 @@ class Installer
             return $exitCode;
         }
 
-        if (method_exists('Composer\Semver\CompilingMatcher', 'clear')) {
+        // exists as of composer/semver 3.3.0
+        if (method_exists('Composer\Semver\CompilingMatcher', 'clear')) { // @phpstan-ignore-line
             \Composer\Semver\CompilingMatcher::clear();
         }
 
