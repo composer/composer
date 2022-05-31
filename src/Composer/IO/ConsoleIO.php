ad4374649a30c72293170d0d653358166ced57eb
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2022-05-31 22:02:54 +1200

    Allow formatting console output as a table.

--- src/Composer/IO/ConsoleIO.php
+++ src/Composer/IO/ConsoleIO.php
@@ -15,6 +15,7 @@ namespace Composer\IO;
 use Composer\Question\StrictConfirmationQuestion;
 use Symfony\Component\Console\Helper\HelperSet;
 use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\ConsoleOutputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -340,6 +341,14 @@ class ConsoleIO extends BaseIO
         }
 
         return $results;
+    }
+
+    /**
+     * @return Table
+     */
+    public function getTable(): Table
+    {
+        return new Table($this->output);
     }
 
     /**
