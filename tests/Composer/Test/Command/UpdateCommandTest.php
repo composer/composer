5888945c302b675c02435aee804d59bc9710236e
Jordi Boggiano <j.boggiano@seld.be>
2022-06-01 22:10:48 +0200

    Fix tests on windows

--- tests/Composer/Test/Command/UpdateCommandTest.php
+++ tests/Composer/Test/Command/UpdateCommandTest.php
@@ -28,7 +28,7 @@ class UpdateCommandTest extends TestCase
         $appTester = $this->getApplicationTester();
         $appTester->run(array_merge(['command' => 'update', '--dry-run' => true], $command));
 
-        $this->assertSame(trim($expected), trim($appTester->getDisplay()));
+        $this->assertSame(strtr(trim($expected), "\r", ''), strtr(trim($appTester->getDisplay()), "\r", ''));
     }
 
     public function provideUpdates(): \Generator
