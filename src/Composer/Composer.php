f14b02b9c9531b31d8547d88f07d493d1730a74c
Jordi Boggiano <j.boggiano@seld.be>
2022-07-01 12:01:26 +0200

    Reverting release version changes

--- src/Composer/Composer.php
+++ src/Composer/Composer.php
@@ -52,10 +52,10 @@ class Composer
      * const RELEASE_DATE = '@release_date@';
      * const SOURCE_VERSION = '1.8-dev+source';
      */
-    const VERSION = '2.2.15';
-    const BRANCH_ALIAS_VERSION = '';
-    const RELEASE_DATE = '2022-07-01 12:01:26';
-    const SOURCE_VERSION = '';
+    const VERSION = '@package_version@';
+    const BRANCH_ALIAS_VERSION = '@package_branch_alias_version@';
+    const RELEASE_DATE = '@release_date@';
+    const SOURCE_VERSION = '2.2.999-dev+source';
 
     /**
      * Version number of the internal composer-runtime-api package
