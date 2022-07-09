7ffd9d04588773293b2fe6967a3445cd59c0ad9f
theoboldalex <theoboldalex@gmail.com>
2022-07-09 19:42:20 +0100

    test: Create test for CheckPlatformReqsCommand

---
+++ tests/Composer/Test/Command/CheckPlatformReqsCommandTest.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of Composer.
+ *
+ * (c) Nils Adermann <naderman@naderman.de>
+ *     Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Composer\Test\Command;
+
+use Composer\Test\TestCase;
+
+class CheckPlatformReqsCommandTest extends TestCase
+{
+    /**
+     * @dataProvider flagGenerator
+     * @param array<mixed> $flag
+     */
+    public function testPlatformReqsAreSatisfied(array $flag): void
+    {
+        $appTester = $this->getApplicationTester();
+        $appTester->run(array_merge(['command' => 'check-platform-reqs'], $flag));
+
+        $appTester->assertCommandIsSuccessful();
+    }
+
+    protected function flagGenerator(): \Generator
+    {
+        yield 'Disables checking of require-dev packages requirements.' => [
+            ['--no-dev' => true]
+        ];
+
+        yield 'Whether to disable plugins.' => [
+            ['--no-plugins' => true]
+        ];
+
+        yield 'Checks requirements only from the lock file, not from installed packages.' => [
+            ['--lock' => true]
+        ];
+    }
+}
