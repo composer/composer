84175907664ca8b73f73f4883e67e886dfefb9f5
Jordi Boggiano <j.boggiano@seld.be>
2022-08-20 11:33:38 +0200

    Release 2.2.18

--- src/Composer/Composer.php
+++ src/Composer/Composer.php
@@ -52,10 +52,10 @@ class Composer
      * const RELEASE_DATE = '@release_date@';
      * const SOURCE_VERSION = '1.8-dev+source';
      */
-    const VERSION = '@package_version@';
-    const BRANCH_ALIAS_VERSION = '@package_branch_alias_version@';
-    const RELEASE_DATE = '@release_date@';
-    const SOURCE_VERSION = '2.2.999-dev+source';
+    const VERSION = '2.2.18';
+    const BRANCH_ALIAS_VERSION = '';
+    const RELEASE_DATE = '2022-08-20 11:33:38';
+    const SOURCE_VERSION = '';
 
     /**
      * Version number of the internal composer-runtime-api package
