cbb4b4bfe4f89d11725fe4f4481a97276e4058fa
Jordi Boggiano <j.boggiano@seld.be>
2022-12-17 23:09:41 +0100

    remove useless require from test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -25,8 +25,6 @@ if (!class_exists(InstalledVersions::class, false)) {
     require __DIR__.'/../src/Composer/InstalledVersions.php';
 }
 
-require __DIR__.'/Composer/Test/TestCase.php';
-
 Platform::putEnv('COMPOSER_TESTS_ARE_RUNNING', '1');
 
 // symfony/phpunit-bridge sets some default env vars which we do not need polluting the test env
