0eec116c861285ed1227ff157d956a164dd830b4
Jordi Boggiano <j.boggiano@seld.be>
2023-01-27 14:49:24 +0100

    Add a CI check to ensure the autoloader can run on php5.6+

---
+++ tests/Composer/Test/Autoload/MinimumVersionSupport/Foo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Test;
+
+class Foo
+{
+	public static function test()
+	{
+		echo 'OK'.PHP_EOL;
+	}
+}
---
+++ tests/Composer/Test/Autoload/MinimumVersionSupport/main.php
@@ -0,0 +1,5 @@
+<?php
+
+include 'vendor/autoload.php';
+
+Test\Foo::test();
