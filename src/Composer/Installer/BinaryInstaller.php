e28a5675b7081b22223072e8afa0a2be36c90fe4
Jordi Boggiano <j.boggiano@seld.be>
2023-05-05 22:24:59 +0200

    Fixed binary proxies to return whatever the original binary returns as well, fixes #11416 (#11454)

--- src/Composer/Installer/BinaryInstaller.php
+++ src/Composer/Installer/BinaryInstaller.php
@@ -337,8 +337,7 @@ if (PHP_VERSION_ID < 80000) {
         (function_exists('stream_get_wrappers') && in_array('phpvfscomposer', stream_get_wrappers(), true))
         || (function_exists('stream_wrapper_register') && stream_wrapper_register('phpvfscomposer', 'Composer\BinProxyWrapper'))
     ) {
-        include("phpvfscomposer://" . $binPathExported);
-        exit(0);
+        return include("phpvfscomposer://" . $binPathExported);
     }
 }
 
@@ -360,7 +359,7 @@ namespace Composer;
 
 $globalsCode
 $streamProxyCode
-include $binPathExported;
+return include $binPathExported;
 
 PROXY;
         }
