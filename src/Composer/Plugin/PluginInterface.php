bc1ed3b3c5c8177173e442bcd496d6fedef805c4
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-06-06 17:03:39 +0200

    chore: bump `PluginInterface::PLUGIN_API_VERSION` to `2.6.0`

--- src/Composer/Plugin/PluginInterface.php
+++ src/Composer/Plugin/PluginInterface.php
@@ -32,7 +32,7 @@ interface PluginInterface
      *
      * @var string
      */
-    public const PLUGIN_API_VERSION = '2.3.0';
+    public const PLUGIN_API_VERSION = '2.6.0';
 
     /**
      * Apply plugin modifications to Composer
