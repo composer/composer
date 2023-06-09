128c2ffdca48b0fe453426b1b90814a99e2c1315
theoboldalex <theoboldalex@gmail.com>
2023-06-09 21:08:46 +0100

    test: we cover the happy path

---
+++ tests/Composer/Test/Command/ReinstallCommandTest.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of Composer.
+ *
+ * (c) Nils Adermann <naderman@naderman.de>
+ *     Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Composer\Test\Command;
+
+use Composer\Test\TestCase;
+
+class ReinstallCommandTest extends TestCase
+{
+    public function testReinstallCommand(): void
+    {
+        $this->initTempComposer(['require' => ['root/req' => '1.*']]);
+
+        $rootReqPackage = self::getPackage('root/req');
+        $rootReqPackage->setType('metapackage');
+
+        $this->createComposerLock([$rootReqPackage], []);
+        $this->createInstalledJson([$rootReqPackage], []);
+
+        $appTester = $this->getApplicationTester();
+        $appTester->run([
+            'command' => 'reinstall',
+            '--no-progress' => true,
+            'packages' => ['root/req']
+        ]);
+
+        $this->assertSame('- Removing root/req (1.0.0)
+  - Installing root/req (1.0.0)', trim($appTester->getDisplay(true)));
+    }
+}
