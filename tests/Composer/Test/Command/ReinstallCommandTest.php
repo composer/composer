ba13db2bf1246d97f1f90fcc45f9ccc36680d20b
theoboldalex <theoboldalex@gmail.com>
2023-06-09 21:18:52 +0100

    test: covers the warnings

--- tests/Composer/Test/Command/ReinstallCommandTest.php
+++ tests/Composer/Test/Command/ReinstallCommandTest.php
@@ -36,4 +36,26 @@ class ReinstallCommandTest extends TestCase
         $this->assertSame('- Removing root/req (1.0.0)
   - Installing root/req (1.0.0)', trim($appTester->getDisplay(true)));
     }
+
+    public function testReinstallCommandWherePackageNotCurrentlyInstalled(): void 
+    {
+        
+        $this->initTempComposer(['require' => ['root/req' => '1.*']]);
+
+        $rootReqPackage = self::getPackage('root/req');
+        $rootReqPackage->setType('metapackage');
+
+        $this->createComposerLock([$rootReqPackage], []);
+        $this->createInstalledJson([$rootReqPackage], []);
+
+        $appTester = $this->getApplicationTester();
+        $appTester->run([
+            'command' => 'reinstall',
+            '--no-progress' => true,
+            'packages' => ['root/anotherreq']
+        ]);
+
+        $this->assertSame('<warning>Pattern "root/anotherreq" does not match any currently installed packages.</warning>
+<warning>Found no packages to reinstall, aborting.</warning>', trim($appTester->getDisplay(true)));
+    }
 }
