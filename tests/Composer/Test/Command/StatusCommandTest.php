a42031aa5f4f3bc54eb090fa7c3b5ddb7d94895c
theoboldalex <theoboldalex@gmail.com>
2023-08-31 23:46:09 +0100

    Unable to get src installed for test
    I have left some debugging and comments in to show
    where the issue is. Most likely is I have misunderstood
    something fundamental about how packages work.
    Any guidance would be hugely appreciated.

--- tests/Composer/Test/Command/StatusCommandTest.php
+++ tests/Composer/Test/Command/StatusCommandTest.php
@@ -16,7 +16,7 @@ use Composer\Test\TestCase;
 
 class StatusCommandTest extends TestCase
 {
-    public function testNoLocalChanges(): void 
+    public function testNoLocalChanges(): void
     {
         $this->initTempComposer(['require' => ['root/req' => '1.*']]);
 
@@ -30,5 +30,39 @@ class StatusCommandTest extends TestCase
         $appTester->run(['command' => 'status']);
 
         $this->assertSame('No local changes', trim($appTester->getDisplay(true)));
+    }
+
+    public function testLocalPackageHasChanges(): void
+    {
+        $this->initTempComposer([
+            'require' => ['composer/class-map-generator' => '^1.1']
+        ]);
+
+        $package = self::getPackage('composer/class-map-generator', '1.1');
+        $package->setInstallationSource('source');
+        $package->setSourceType('git');
+        $package->setSourceUrl('https://github.com/composer/class-map-generator.git');
+        $package->setSourceReference('953cc4ea32e0c31f2185549c7d216d7921f03da9');
+
+        $this->createComposerLock([$package], []);
+        $this->createInstalledJson([$package], []);
+
+        // installed.json LGTM
+        var_dump(file_get_contents(getcwd() . '/vendor/composer/installed.json'));
+        // Expected some source here instead of an empty dir
+        var_dump(scandir(getcwd() . '/vendor/composer/class-map-generator/'));
+
+        // A hack to modify a package file just to get the test stood up. Will find a better way...
+        file_put_contents(getcwd() . '/vendor/composer/class-map-generator/composer.json', '{}');
+
+        $appTester = $this->getApplicationTester();
+        $appTester->run(['command' => 'status']);
+
+        $expected = '<error>You have changes in the following dependencies:</error>';
+        $actual = trim($appTester->getDisplay(true));
+        $modifiedLocalPackage = 'class-map-generator';
+
+        $this->assertStringContainsString($expected, $actual);
+        $this->assertStringContainsString($modifiedLocalPackage, $actual);
     }
 }
