5c511bd437815c83dbae826a291fa77195b55625
Jordi Boggiano <j.boggiano@seld.be>
2024-01-08 14:41:29 +0100

    Fix warnings incorrectly being shown when using require with upper bound ignored on platform requirements, fixes #11722

--- tests/Composer/Test/Filter/PlatformRequirementFilter/IgnoreAllPlatformRequirementFilterTest.php
+++ tests/Composer/Test/Filter/PlatformRequirementFilter/IgnoreAllPlatformRequirementFilterTest.php
@@ -25,6 +25,7 @@ final class IgnoreAllPlatformRequirementFilterTest extends TestCase
         $platformRequirementFilter = new IgnoreAllPlatformRequirementFilter();
 
         $this->assertSame($expectIgnored, $platformRequirementFilter->isIgnored($req));
+        $this->assertSame($expectIgnored, $platformRequirementFilter->isUpperBoundIgnored($req));
     }
 
     /**
