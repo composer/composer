ebb6a82099cc8b97765ecca627c5bc4ff8b0315b
Derek Stephen McLean <delboy1978uk@gmail.com>
2024-02-06 16:53:18 +0100

    issue #11811 auth token links on separate lines (#11812)
    * issue #11811 auth token links on separate lines
    
    * 11811 - remove stray bracket
    
    * 11811 : links on separte lines

--- src/Composer/Util/Bitbucket.php
+++ src/Composer/Util/Bitbucket.php
@@ -143,7 +143,8 @@ class Bitbucket
 
         $localAuthConfig = $this->config->getLocalAuthConfigSource();
         $url = 'https://support.atlassian.com/bitbucket-cloud/docs/use-oauth-on-bitbucket-cloud/';
-        $this->io->writeError(sprintf('Follow the instructions on %s', $url));
+        $this->io->writeError('Follow the instructions here:');
+        $this->io->writeError($url);
         $this->io->writeError(sprintf('to create a consumer. It will be stored in "%s" for future use by Composer.', ($localAuthConfig !== null ? $localAuthConfig->getName() . ' OR ' : '') . $this->config->getAuthConfigSource()->getName()));
         $this->io->writeError('Ensure you enter a "Callback URL" (http://example.com is fine) or it will not be possible to create an Access Token (this callback url will not be used by composer)');
 
