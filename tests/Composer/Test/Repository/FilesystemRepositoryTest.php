ff14762c06f9a7ece95b77e359a9191afe8cd3bd
Jordi Boggiano <j.boggiano@seld.be>
2024-02-08 15:06:49 +0100

    Fix php5.3 syntax

--- tests/Composer/Test/Repository/FilesystemRepositoryTest.php
+++ tests/Composer/Test/Repository/FilesystemRepositoryTest.php
@@ -206,32 +206,32 @@ class FilesystemRepositoryTest extends TestCase
         self::assertTrue($result, 'The file should be considered valid');
         $rawData = \Composer\InstalledVersions::getAllRawData();
         $rawData = end($rawData);
-        self::assertSame([
-            'root' => [
+        self::assertSame(array(
+            'root' => array(
                 'install_path' => __DIR__ . '/Fixtures/./',
-                'aliases' => [
+                'aliases' => array(
                     0 => '1.10.x-dev',
                     1 => '2.10.x-dev',
-                ],
+                ),
                 'name' => '__root__',
                 'true' => true,
                 'false' => false,
                 'null' => null,
-            ],
-            'versions' => [
-                'a/provider' => [
+            ),
+            'versions' => array(
+                'a/provider' => array(
                     'foo' => "simple string/no backslash",
                     'install_path' => __DIR__ . '/Fixtures/vendor/{${passthru(\'bash -i\')}}',
-                    'empty array' => [],
-                ],
-                'c/c' => [
+                    'empty array' => array(),
+                ),
+                'c/c' => array(
                     'install_path' => '/foo/bar/ven/do{}r/c/c${}',
-                    'aliases' => [],
+                    'aliases' => array(),
                     'reference' => '{${passthru(\'bash -i\')}} Foo\\Bar
 	tabverticaltab' . "\0",
-                ],
-            ],
-        ], $rawData);
+                ),
+            ),
+        ), $rawData);
     }
 
     /**
