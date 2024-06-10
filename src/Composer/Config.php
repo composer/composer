fa3b9582c38fa2b7ed14989101a5d1e1788d787c
Jordi Boggiano <j.boggiano@seld.be>
2024-06-10 14:48:02 +0200

    Fix secure-http check to avoid bypass using emojis

--- src/Composer/Config.php
+++ src/Composer/Config.php
@@ -584,8 +584,8 @@ class Config
      */
     public function prohibitUrlByConfig(string $url, ?IOInterface $io = null, array $repoOptions = []): void
     {
-        // Return right away if the URL is malformed or custom (see issue #5173)
-        if (false === filter_var($url, FILTER_VALIDATE_URL)) {
+        // Return right away if the URL is malformed or custom (see issue #5173), but only for non-HTTP(S) URLs
+        if (false === filter_var($url, FILTER_VALIDATE_URL) && !Preg::isMatch('{^https?://}', $url)) {
             return;
         }
 
