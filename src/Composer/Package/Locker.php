5f2b91aea8d81810133c6d40f170fcbe38150476
Jordi Boggiano <j.boggiano@seld.be>
2024-09-16 11:31:52 +0200

    Revert "fix(Locker): don't store transport-options.ssl within the lock-file (#12019)"
    This reverts commit 03bbfdd8f4d6ce3c0678b6cba26446704b4d594b.

--- src/Composer/Package/Locker.php
+++ src/Composer/Package/Locker.php
@@ -434,14 +434,6 @@ class Locker
 
             $spec = $this->dumper->dump($package);
             unset($spec['version_normalized']);
-            // remove `transport-options.ssl` from lock file to prevent storing
-            // local-filesystem repo config paths in the lock file as that makes it less portable
-            if (isset($spec['transport-options']['ssl'])) {
-                unset($spec['transport-options']['ssl']);
-                if (\count($spec['transport-options']) === 0) {
-                    unset($spec['transport-options']);
-                }
-            }
 
             // always move time to the end of the package definition
             $time = $spec['time'] ?? null;
