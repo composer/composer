d5e75c21ceeb6829918a75ed63b56c576b4a6261
Jordi Boggiano <j.boggiano@seld.be>
2024-10-02 16:40:29 +0200

    Release 2.8.0

--- src/Composer/Composer.php
+++ src/Composer/Composer.php
@@ -51,10 +51,10 @@ class Composer extends PartialComposer
      *
      * @see getVersion()
      */
-    public const VERSION = '@package_version@';
-    public const BRANCH_ALIAS_VERSION = '@package_branch_alias_version@';
-    public const RELEASE_DATE = '@release_date@';
-    public const SOURCE_VERSION = '2.8.999-dev+source';
+    public const VERSION = '2.8.0';
+    public const BRANCH_ALIAS_VERSION = '';
+    public const RELEASE_DATE = '2024-10-02 16:40:29';
+    public const SOURCE_VERSION = '';
 
     /**
      * Version number of the internal composer-runtime-api package
