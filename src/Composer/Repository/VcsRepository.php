5b256070b7c494f5d9a2554128b51739220a7de8
Jordi Boggiano <j.boggiano@seld.be>
2024-10-02 13:26:08 +0200

    Tweak output of VcsRepo to be less verbose

--- src/Composer/Repository/VcsRepository.php
+++ src/Composer/Repository/VcsRepository.php
@@ -217,11 +217,6 @@ class VcsRepository extends ArrayRepository implements ConfigurableRepositoryInt
         foreach ($driver->getTags() as $tag => $identifier) {
             $tag = (string) $tag;
             $msg = 'Reading composer.json of <info>' . ($this->packageName ?: $this->url) . '</info> (<comment>' . $tag . '</comment>)';
-            if ($isVeryVerbose) {
-                $this->io->writeError($msg);
-            } elseif ($isVerbose) {
-                $this->io->overwriteError($msg, false);
-            }
 
             // strip the release- prefix from tags if present
             $tag = str_replace('release-', '', $tag);
@@ -243,6 +238,12 @@ class VcsRepository extends ArrayRepository implements ConfigurableRepositoryInt
                     $this->io->writeError('<warning>Skipped tag '.$tag.', invalid tag name</warning>');
                 }
                 continue;
+            }
+
+            if ($isVeryVerbose) {
+                $this->io->writeError($msg);
+            } elseif ($isVerbose) {
+                $this->io->overwriteError($msg, false);
             }
 
             try {
