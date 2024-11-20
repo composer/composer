f48be18bace290d4a7080a2085dd22a8d7cad29a
Stephan Vock <stephan.vock@gmail.com>
2024-11-20 17:11:00 +0000

    GitLab: adjust links to profile/user-settings

--- src/Composer/Util/GitLab.php
+++ src/Composer/Util/GitLab.php
@@ -126,8 +126,8 @@ class GitLab
         }
 
         $localAuthConfig = $this->config->getLocalAuthConfigSource();
-        $personalAccessTokenLink = $scheme.'://'.$originUrl.'/-/profile/personal_access_tokens';
-        $revokeLink = $scheme.'://'.$originUrl.'/-/profile/applications';
+        $personalAccessTokenLink = $scheme.'://'.$originUrl.'/-/user_settings/personal_access_tokens';
+        $revokeLink = $scheme.'://'.$originUrl.'/-/user_settings/applications';
         $this->io->writeError(sprintf('A token will be created and stored in "%s", your password will never be stored', ($localAuthConfig !== null ? $localAuthConfig->getName() . ' OR ' : '') . $this->config->getAuthConfigSource()->getName()));
         $this->io->writeError('To revoke access to this token you can visit:');
         $this->io->writeError($revokeLink);
@@ -312,7 +312,7 @@ class GitLab
 
         $token = $this->httpDownloader->get($scheme.'://'.$originUrl.'/oauth/token', $options)->decodeJson();
         $this->io->writeError('GitLab token successfully refreshed', true, IOInterface::VERY_VERBOSE);
-        $this->io->writeError('To revoke access to this token you can visit '.$scheme.'://'.$originUrl.'/-/profile/applications', true, IOInterface::VERY_VERBOSE);
+        $this->io->writeError('To revoke access to this token you can visit '.$scheme.'://'.$originUrl.'/-/user_settings/applications', true, IOInterface::VERY_VERBOSE);
 
         return $token;
     }
