fc7eca4a5ce0adb6747d325345611ffda79ac4d2
Brian Henry <BrianHenryIE@gmail.com>
2025-05-01 22:05:38 -0700

    Remove `false` check which no longer can evaluate to false

--- src/Composer/Autoload/AutoloadGenerator.php
+++ src/Composer/Autoload/AutoloadGenerator.php
@@ -1305,9 +1305,7 @@ INITIALIZER;
                         }
 
                         $resolvedPath = Platform::realpath($installPath . '/' . $updir);
-                        if (false === $resolvedPath) {
-                            continue;
-                        }
+
                         $autoloads[] = preg_quote(strtr($resolvedPath, '\\', '/')) . '/' . $path . '($|/)';
                         continue;
                     }
