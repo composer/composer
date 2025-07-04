f2d9fd7a71808b0be4dbfc798a7e40a75912ecbb
Brian Henry <BrianHenryIE@gmail.com>
2025-07-03 20:40:02 -0700

    Add `::testRealPathFileStreamStripsScheme()`

--- tests/Composer/Test/Util/PlatformTest.php
+++ tests/Composer/Test/Util/PlatformTest.php
@@ -108,6 +108,13 @@ class PlatformTest extends TestCase
         Platform::getBoolEnv('COMPOSER_TEST_BOOL_ENV');
     }
 
+    public function testRealPathFileStreamStripsScheme(): void
+    {
+        $file = __FILE__;
+        $streamPath = 'file://' . $file;
+        self::assertEquals($file, Platform::realpath($streamPath));
+    }
+
     public function testRealPathException(): void
     {
         // Test that `::realpath()` throws an exception on a non-existing path
