c5ea643fd0878162057603e09fb912783fd80ad7
Jordi Boggiano <j.boggiano@seld.be>
2025-11-07 14:47:19 +0100

    Reverting release version changes

--- src/Composer/Composer.php
+++ src/Composer/Composer.php
@@ -51,10 +51,10 @@ class Composer extends PartialComposer
      *
      * @see getVersion()
      */
-    public const VERSION = '2.9.0-RC1';
-    public const BRANCH_ALIAS_VERSION = '';
-    public const RELEASE_DATE = '2025-11-07 14:47:19';
-    public const SOURCE_VERSION = '';
+    public const VERSION = '@package_version@';
+    public const BRANCH_ALIAS_VERSION = '@package_branch_alias_version@';
+    public const RELEASE_DATE = '@release_date@';
+    public const SOURCE_VERSION = '2.9.999-dev+source';
 
     /**
      * Version number of the internal composer-runtime-api package
