47cde53a71f812525489c0b4bd2a01b0698c530f
Jordi Boggiano <j.boggiano@seld.be>
2025-12-29 15:01:13 +0100

    Fix support for securetransport + libressl
    Fixes #12615

--- src/Composer/Repository/PlatformRepository.php
+++ src/Composer/Repository/PlatformRepository.php
@@ -240,12 +240,14 @@ class PlatformRepository extends ArrayRepository
                             $parsedVersion = Version::parseOpenssl($sslMatches['version'], $isFips);
                             $this->addLibrary($libraries, $name.'-openssl'.($isFips ? '-fips' : ''), $parsedVersion, 'curl OpenSSL version ('.$parsedVersion.')', [], $isFips ? ['curl-openssl'] : []);
                         } else {
-                            if ($library === '(securetransport) openssl') {
+                            if (str_starts_with($library, '(securetransport)') && Preg::isMatch('{^\(securetransport\) ([a-z0-9]+)}', $library, $securetransportMatches)) {
                                 $shortlib = 'securetransport';
+                                $sslLib = 'curl-'.$securetransportMatches[1];
                             } else {
                                 $shortlib = $library;
+                                $sslLib = 'curl-openssl';
                             }
-                            $this->addLibrary($libraries, $name.'-'.$shortlib, $sslMatches['version'], 'curl '.$library.' version ('.$sslMatches['version'].')', ['curl-openssl']);
+                            $this->addLibrary($libraries, $name.'-'.$shortlib, $sslMatches['version'], 'curl '.$library.' version ('.$sslMatches['version'].')', [$sslLib]);
                         }
                     }
 
