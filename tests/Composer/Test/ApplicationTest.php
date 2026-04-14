composer/composer

commit f8a87966f77267f041949ca9785df6992ec4a128

Fix inconsistent treatment of SingleCommandApplication script commands (#12758)
They are now always handled the same regardless if plugins are present and now warn about being incompatible.

Fixes #12749

src/Composer/Console/Application.php
@@ -43,6 +43,7 @@ use Composer\Util\HttpDownloader;
 use Composer\EventDispatcher\ScriptExecutionException;
 use Composer\Exception\NoSslException;
 use Composer\XdebugHandler\XdebugHandler;
+use Symfony\Component\Console\SingleCommandApplication;
 use Symfony\Component\Process\Exception\ProcessTimedOutException;
 
 /**
@@ -260,6 +261,8 @@ class Application extends BaseApplication
                 || ($commandName === '_complete' && !$isNonAllowedRoot)
             );
 
+        $mayNeedScriptCommand = $mayNeedPluginCommand || $commandName === 'run-script';
+
         if ($mayNeedPluginCommand && !$this->disablePluginsByDefault && !$this->hasPluginCommands) {
             // at this point plugins are needed, so if we are running as root and it is not allowed we need to prompt
             // if interactive, and abort otherwise
@@ -368,36 +371,52 @@ class Application extends BaseApplication
 
             // add non-standard scripts as own commands
             $file = Factory::getComposerFile();
-            if (is_file($file) && Filesystem::isReadable($file) && is_array($composer = json_decode(file_get_contents($file), true))) {
-                if (isset($composer['scripts']) && is_array($composer['scripts'])) {
-                    foreach ($composer['scripts'] as $script => $dummy) {
+            if ($mayNeedScriptCommand && is_file($file) && Filesystem::isReadable($file) && is_array($composerJson = json_decode(file_get_contents($file), true))) {
+                if (isset($composerJson['scripts']) && is_array($composerJson['scripts'])) {
+                    foreach ($composerJson['scripts'] as $script => $dummy) {
                         if (!defined('Composer\Script\ScriptEvents::'.str_replace('-', '_', strtoupper($script)))) {
                             if ($this->has($script)) {
                                 $io->writeError('<warning>A script named '.$script.' would override a Composer command and has been skipped</warning>');
                             } else {
-                                $description = null;
+                                $description = 'Runs the '.$script.' script as defined in composer.json';
 
-                                if (isset($composer['scripts-descriptions'][$script])) {
-                                    $description = $composer['scripts-descriptions'][$script];
+                                if (isset($composerJson['scripts-descriptions'][$script])) {
+                                    $description = $composerJson['scripts-descriptions'][$script];
                                 }
 
-                                $aliases = $composer['scripts-aliases'][$script] ?? [];
+                                $aliases = $composerJson['scripts-aliases'][$script] ?? [];
 
-                                //if the command is not an array of commands, and points to a valid Command subclass, import its details directly
+                                $composer = $this->getComposer(false, false);
+                                if ($composer !== null) {
+                                    $rootPackage = $composer->getPackage();
+                                    $generator = $composer->getAutoloadGenerator();
+
+                                    $packageMap = $generator->buildPackageMap($composer->getInstallationManager(), $rootPackage, [$rootPackage]);
+                                    $map = $generator->parseAutoloads($packageMap, $rootPackage);
+
+                                    $loader = $generator->createLoader($map, $composer->getConfig()->get('vendor-dir'));
+                                    $loader->register(false);
+                                }
+
+                                // if the command is not an array of commands, and points to a valid Command subclass, import its details directly
                                 if (is_string($dummy) && class_exists($dummy) && is_subclass_of($dummy, SymfonyCommand::class)) {
+                                    if (is_subclass_of($dummy, SingleCommandApplication::class)) {
+                                        $io->writeError('<warning>The script named '.$script.' extends SingleCommandApplication which is not compatible with Composer 2.9+, make sure you extend Symfony\Component\Console\Command instead.</warning>');
+                                    }
+
                                     $cmd = new $dummy($script);
 
-                                    //makes sure the command is find()'able by the name defined in composer.json, and the name isn't overridden in its configure()
+                                    // makes sure the command is find()'able by the name defined in composer.json, and the name isn't overridden in its configure()
                                     if ($cmd->getName() !== '' && $cmd->getName() !== null && $cmd->getName() !== $script) {
                                         $io->writeError('<warning>The script named '.$script.' in composer.json has a mismatched name in its class definition. For consistency, either use the same name, or do not define one inside the class.</warning>');
-                                        $cmd->setName($script); //override it with the defined script name
+                                        $cmd->setName($script); // override it with the defined script name
                                     }
 
                                     if ($cmd->getDescription() === '' && is_string($description)) {
                                         $cmd->setDescription($description);
                                     }
                                 } else {
-                                    //fallback to usual aliasing behavior
+                                    // fallback to usual aliasing behavior
                                     $cmd = new Command\ScriptAliasCommand($script, $description, $aliases);
                                 }
 

commit ca0612e27a327aa5e1b27503ec32d1f7dab1fc10

Fixes custom script command aliases regression when a script is called a substring of a composer command
Regression from the optimization in https://github.com/composer/composer/pull/12758 - Fixes #12802

src/Composer/Console/Application.php
@@ -176,9 +176,9 @@ class Application extends BaseApplication
 
         // determine command name to be executed without including plugin commands
         $commandName = '';
-        if ($name = $this->getCommandNameBeforeBinding($input)) {
+        if ($rawCommandName = $this->getCommandNameBeforeBinding($input)) {
             try {
-                $commandName = $this->find($name)->getName();
+                $commandName = $this->find($rawCommandName)->getName();
             } catch (CommandNotFoundException $e) {
                 // we'll check command validity again later after plugins are loaded
                 $commandName = false;
@@ -261,7 +261,7 @@ class Application extends BaseApplication
                 || ($commandName === '_complete' && !$isNonAllowedRoot)
             );
 
-        $mayNeedScriptCommand = $mayNeedPluginCommand || $commandName === 'run-script';
+        $mayNeedScriptCommand = $mayNeedPluginCommand || $commandName === 'run-script' || $rawCommandName !== $commandName;
 
         if ($mayNeedPluginCommand && !$this->disablePluginsByDefault && !$this->hasPluginCommands) {
             // at this point plugins are needed, so if we are running as root and it is not allowed we need to prompt

tests/Composer/Test/ApplicationTest.php
@@ -123,4 +123,31 @@ class ApplicationTest extends TestCase
         self::assertTrue($composer->getPluginManager()->arePluginsDisabled('local'), 'Plugins should be disabled when --no-plugins is used');
         self::assertTrue($composer->getPluginManager()->arePluginsDisabled('global'), 'Global plugins should be disabled when --no-plugins is used');
     }
+
+    /**
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     * @see https://github.com/composer/composer/issues/12802
+     */
+    public function testScriptCommandTakesPriorityOverAbbreviatedBuiltinCommand(): void
+    {
+        $this->initTempComposer([
+            'scripts' => [
+                'check' => 'echo hello',
+            ],
+        ]);
+
+        $application = new Application();
+        $application->setAutoExit(false);
+        $application->setCatchExceptions(false);
+        if (method_exists($application, 'setCatchErrors')) {
+            $application->setCatchErrors(false);
+        }
+
+        $appOutput = new BufferedOutput();
+        $exitCode = $application->doRun(new ArrayInput(['command' => 'check']), $appOutput);
+
+        self::assertSame(0, $exitCode, 'Script command should have run successfully');
+        self::assertStringContainsString('hello', $appOutput->fetch(), 'The "check" script should have been executed instead of the check-platform-reqs command');
+    }
 }